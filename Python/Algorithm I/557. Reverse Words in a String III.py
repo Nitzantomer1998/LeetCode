4f4937000e3dfb9c141eb82b763412ec99bd00b3
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-01-20 08:16:11 +0200

    Adjusting notes

--- Python/Algorithm I/557. Reverse Words in a String III.py
+++ Python/Algorithm I/557. Reverse Words in a String III.py
@@ -1,7 +1,18 @@
 def reverse_words(string: str) -> str:
-    
+    """
+    Reversing the order of characters in each word within a sentence, and return it
+
+    :param string: String represent a sentence
+    :return: The sentence which every word is reversed
+
+    Time Complexity: o(n)
+    Space Complexity: o(n)
+    """
+    # Creating list, which every index of her hold a full word from string
     words = string.split()
 
+    # Reversing every word in words
     words = [word[::-1] for word in words]
 
+    # Returning the reversed string, by adding spacebar between every two words
     return ' '.join(words)
