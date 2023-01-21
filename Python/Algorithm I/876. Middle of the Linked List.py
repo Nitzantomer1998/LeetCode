789ed9ef5dd31f7ed0e2652801636949b97df266
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-01-21 09:02:29 +0200

    Adjusting notes

--- Python/Algorithm I/876. Middle of the Linked List.py
+++ Python/Algorithm I/876. Middle of the Linked List.py
@@ -1,3 +1,4 @@
+# Class provided by LeetCode for the following problem
 class ListNode:
     def __init__(self, value=0, next=None):
         self.value = value
@@ -5,12 +6,26 @@ class ListNode:
 
 
 def middle_node(head: ListNode) -> ListNode:
-    
+    """
+    Iterate the ListNode, and return the middle node of the sent ListNode
+
+    :param head: ListNode
+    :return: The middle node of the sent ListNode
+
+    Time Complexity: o(n)
+    Space Complexity: o(1)
+    """
+    # Storing the middle node (the solution)
     solution_node = head
 
+    # Loop to traverse the ListNode
+    # Note: every 2 steps forwards of head node, will require the solution node to move forward once
     while head and head.next:
+        # Updating the head, to jump 2 steps forwards
         head = head.next.next
 
+        # Updating the solution node to move 1 step forward
         solution_node = solution_node.next
 
+    # Returning the solution node
     return solution_node
