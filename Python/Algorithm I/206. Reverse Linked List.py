edbb13c1a72e8122cffa88f4f533b6bbbe903c7d
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-01-26 07:52:30 +0200

    Adjusting notes

--- Python/Algorithm I/206. Reverse Linked List.py
+++ Python/Algorithm I/206. Reverse Linked List.py
@@ -1,3 +1,4 @@
+# Class provided by LeetCode for the following problem
 class ListNode:
     def __init__(self, value=0, next=None):
         self.value = value
@@ -5,17 +6,32 @@ class ListNode:
 
 
 def reverse_list(head: ListNode) -> ListNode:
-  
+    """
+    Reversing the ListNode, and return it
+
+    :param head: ListNode
+    :return: The reversed ListNode
+
+    Time Complexity: o(n)
+    Space Complexity: o(1)
+    """
+    # Storing the current node
     current_node = head
 
+    # Storing the previous node
     previous_node = None
 
+    # Loop to traverse the ListNode
     while current_node:
+        # Save the next node
         next_node = current_node.next
 
+        # Update the current_node next pointer to be the previous node
         current_node.next = previous_node
 
+        # Update the previous and current nodes, for the next iteration
         previous_node = current_node
         current_node = next_node
 
+    # Return the new head of the ListNode
     return previous_node
