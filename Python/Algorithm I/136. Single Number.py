383bd6cd71145f9b0e635213d6be225aec397de1
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-01-30 21:33:39 +0200

    Adjusting notes

--- Python/Algorithm I/136. Single Number.py
+++ Python/Algorithm I/136. Single Number.py
@@ -1,9 +1,22 @@
 def single_number(numbers: list[int]) -> int:
-    
+    """
+    Finding the unique element in am array which every element appears twice except for one. and return it.
+
+    :param numbers: List of integers
+    :return: The unique number
+
+    Time Complexity: o(n)
+    Space Complexity: o(1)
+    """
+    # Variable to store the solution
     single_number_solution = 0
 
+    # Loop to traverse all the numbers
     for value in numbers:
-        
+        # Update the single number solution by making XOR operator with all the available numbers
+        # Note : XOR operator return 1 only if the two bits values are difference, else it will be 0
+        # Therefor only when we hit the True single number, the bits wouldn't change till we finished
         single_number_solution ^= value
 
+    # Return the single number solution
     return single_number_solution
