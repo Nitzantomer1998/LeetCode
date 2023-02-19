93a43d2742987dc37bd7cd1f47b8b610894ac7e8
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-02-19 08:45:49 +0200

    Adding Day 18 Problem 343

---
+++ Python/Algorithm/Algorithm II/343. Integer Break.py
@@ -0,0 +1,10 @@
+def integer_break(n: int) -> int:
+   
+    MP = [value for value in range(n + 1)]
+    MP[-1] = 0
+
+    for number in range(2, n + 1):
+        for index in range(1, number):
+            MP[number] = max(MP[number], MP[index] * MP[number - index])
+
+    return MP[-1]
