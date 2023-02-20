e3d9465fa4924f28fa8175307749afca8f198da9
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-02-20 08:51:51 +0200

    Adjusting notes

--- Python/Algorithm/Algorithm II/201. Bitwise AND of Numbers Range.py
+++ Python/Algorithm/Algorithm II/201. Bitwise AND of Numbers Range.py
@@ -1,11 +1,25 @@
 def range_bitwise_and(left: int, right: int) -> int:
-    
+    """
+    Finding the bitwise AND of all numbers in the range [left, right], and return it
+
+    :param left: Integer represent the left boundary
+    :param right: Integer represent the right boundary
+    :return: The bitwise AND of all numbers in the range [left, right]
+
+    Time Complexity: o(n)
+    Space Complexity: o(1)
+    """
+    # Integer counter for the amount of rotate shift right we did
     shift_right_counter = 0
 
+    # Loop which run till the bits are the same
     while left != right:
+        # Shifting right for the next iteration
         left >>= 1
         right >>= 1
 
+        # Updating the shift right rotate counter
         shift_right_counter += 1
 
+    # Returning the bitwise AND of all numbers in the range [left, right]
     return left << shift_right_counter
