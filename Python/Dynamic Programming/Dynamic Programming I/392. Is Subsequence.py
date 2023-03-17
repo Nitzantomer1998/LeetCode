800e821f0175388b980a518d69c80bfdda6f5575
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-03-17 09:08:11 +0200

    Adding Day 19 Problem 392

---
+++ Python/Dynamic Programming/Dynamic Programming I/392. Is Subsequence.py
@@ -0,0 +1,11 @@
+def is_subsequence(s: str, t: str) -> bool:
+    
+    s_ptr = 0
+    t_ptr = 0
+
+    while s_ptr < len(s) and t_ptr < len(t):
+        if s[s_ptr] == t[t_ptr]:
+            s_ptr += 1
+        t_ptr += 1
+
+    return s_ptr == len(s)
