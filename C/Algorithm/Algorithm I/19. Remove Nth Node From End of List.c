a0f77477d027f3f07ab7b6ab3e356f0ce1fe153d
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-04 09:45:02 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/19. Remove Nth Node From End of List.c
+++ C/Algorithm/Algorithm I/19. Remove Nth Node From End of List.c
@@ -1,3 +1,27 @@
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+/*
+ * Computes the length of a singly-linked list.
+ *
+ * The 'getNodeListLength' function calculates the number of nodes in a singly-linked
+ * list by traversing the list and incrementing the length counter for each node
+ * encountered. It takes a pointer to the head of the list as a parameter.
+ *
+ * Parameters:
+ * - head: A pointer to the head node of the singly-linked list.
+ *
+ * Returns:
+ * The length of the singly-linked list.
+ *
+ * This function traverses the linked list while counting the nodes and returns the
+ * total number of nodes in the list.
+ */
 int getNodeListLength(struct ListNode* head) {
 	struct ListNode* traverse = head;
 	int listNodeLength = 0;
@@ -10,6 +34,27 @@ int getNodeListLength(struct ListNode* head) {
 	return listNodeLength;
 }
 
+/*
+ * Removes the nth node from the end of a singly-linked list.
+ *
+ * The 'removeNthFromEnd' function removes the nth node from the end of a singly-linked
+ * list. It calculates the length of the list using the 'getNodeListLength' function and
+ * determines the index of the node to be removed. It then traverses the list to find the
+ * node before the one to be removed and adjusts the 'next' pointers to skip the targeted
+ * node.
+ *
+ * Parameters:
+ * - head: A pointer to the head node of the singly-linked list.
+ * - n: The index of the node to be removed from the end (1-based index).
+ *
+ * Returns:
+ * A pointer to the head of the modified singly-linked list after removing the nth node
+ * from the end.
+ *
+ * Special cases are handled: if the list has only one node and n is 1, the function
+ * returns NULL. If n equals the length of the list, the function returns the second
+ * node as the new head.
+ */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
     struct ListNode* traverse = head;
 	int listNodeLength = getNodeListLength(head);
