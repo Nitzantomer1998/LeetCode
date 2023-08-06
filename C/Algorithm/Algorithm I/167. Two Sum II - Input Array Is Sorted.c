b276831726d4b9b52501226432e6a7bf328f783c
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-06 18:57:59 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/167. Two Sum II - Input Array Is Sorted.c
+++ C/Algorithm/Algorithm I/167. Two Sum II - Input Array Is Sorted.c
@@ -1,3 +1,28 @@
+/*
+ * Finds the indices of two numbers in a sorted array that add up to a target value.
+ *
+ * The 'twoSum' function utilizes a two-pointer approach to find a pair of indices that
+ * correspond to two numbers in the sorted input array, such that their sum matches the
+ * given target value. It initializes two pointers, 'leftPointer' and 'rightPointer', at
+ * the beginning and end of the array, respectively. The function calculates the sum of
+ * the numbers pointed to by the two pointers and adjusts the pointers based on whether
+ * the sum is greater or less than the target. This process continues until the sum equals
+ * the target value, at which point the function returns the indices of the two numbers.
+ *
+ * Parameters:
+ * - numbers: A pointer to a sorted array of integers.
+ * - numbersSize: The number of elements in the array.
+ * - target: The target sum to find.
+ * - returnSize: A pointer to an integer that will store the size of the returned array.
+ *
+ * Returns:
+ * A dynamically allocated integer array containing two indices whose corresponding
+ * numbers add up to the target value. The caller is responsible for freeing the memory
+ * allocated for the returned array.
+ *
+ * Note: The function assumes that there is exactly one solution, and the input array is
+ * sorted in non-decreasing order.
+ */
 int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
     int *indicesSolution = malloc(2 * sizeof(int));
     
