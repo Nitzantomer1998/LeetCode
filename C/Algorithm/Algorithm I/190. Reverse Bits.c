aa6a43523d8c0ac16e0a78d2c7832b66edcf6118
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-07 17:53:24 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/190. Reverse Bits.c
+++ C/Algorithm/Algorithm I/190. Reverse Bits.c
@@ -1,3 +1,23 @@
+/*
+ * Reverses the bits of a 32-bit unsigned integer.
+ *
+ * The 'reverseBits' function takes a 32-bit unsigned integer 'n' as input and reverses
+ * its binary representation to obtain a new unsigned integer. It iterates through each
+ * bit of the input integer from the least significant bit (LSB) to the most significant
+ * bit (MSB). During each iteration, it appends the current bit to the result after
+ * shifting the result one position to the left (multiplying by 2). After processing all
+ * bits, the function returns the reversed integer.
+ *
+ * Parameters:
+ * - n: A 32-bit unsigned integer to be reversed.
+ *
+ * Returns:
+ * A new 32-bit unsigned integer with its bits reversed.
+ *
+ * This function efficiently reverses the bits of the input integer by considering each
+ * bit's position and appending it to the result. The final result represents the binary
+ * reversal of the input integer.
+ */
 uint32_t reverseBits(uint32_t n) {
     int result = 0;
     
