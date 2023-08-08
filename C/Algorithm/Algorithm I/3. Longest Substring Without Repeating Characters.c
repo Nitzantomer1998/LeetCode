30441fe9cfe29411e6020626b7abf114843362b4
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-08 09:29:15 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/3. Longest Substring Without Repeating Characters.c
+++ C/Algorithm/Algorithm I/3. Longest Substring Without Repeating Characters.c
@@ -1,3 +1,26 @@
+/*
+ * Calculates the length of the longest substring without repeating characters.
+ *
+ * The 'lengthOfLongestSubstring' function calculates the length of the longest substring
+ * in a given string 's' that does not contain any repeating characters. It iterates
+ * through the string using a sliding window approach, maintaining a substring defined by
+ * 'substringStartIndex' and 'substringEndIndex'. It uses an integer array 'currentSubstring'
+ * to keep track of the frequency of characters in the current substring. If a character is
+ * already present in the current substring, the function slides the window by incrementing
+ * 'substringStartIndex' until the repeating character is removed from the substring. The
+ * function also updates the 'maxSubstringLength' variable with the length of the longest
+ * non-repeating substring encountered during the iteration.
+ *
+ * Parameters:
+ * - s: A pointer to a null-terminated string.
+ *
+ * Returns:
+ * The length of the longest substring without repeating characters.
+ *
+ * This function efficiently calculates the length of the longest non-repeating substring by
+ * utilizing a sliding window approach and keeping track of character frequencies. It returns
+ * the length of the longest substring found in the input string 's'.
+ */
 int lengthOfLongestSubstring(char * s) {
     int stringLength = strlen(s);
     int maxSubstringLength = 0;
