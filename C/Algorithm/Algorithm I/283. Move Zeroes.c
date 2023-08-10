a465199b65f30c97a2fb728f65d5a0cd19e5de0a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-10 11:25:16 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/283. Move Zeroes.c
+++ C/Algorithm/Algorithm I/283. Move Zeroes.c
@@ -1,3 +1,21 @@
+/*
+ * Moves all zeros in an array to the end while maintaining the relative order of other elements.
+ *
+ * The 'moveZeroes' function rearranges the elements of an integer array 'nums' to move all
+ * occurrences of zeros to the end of the array, while keeping the relative order of the
+ * non-zero elements unchanged. It does so by iterating through the array and using a
+ * 'zeroIndex' to keep track of the position where non-zero elements should be placed. For
+ * each non-zero element, it swaps the current element with the element at 'zeroIndex' and
+ * increments 'zeroIndex'. After processing all elements, it fills the remaining positions
+ * with zeros to ensure all zeros are moved to the end of the array.
+ *
+ * Parameters:
+ * - nums: A pointer to an array of integers.
+ * - numsSize: The number of elements in the array.
+ *
+ * This function modifies the input 'nums' array in place to move all zeros to the end while
+ * maintaining the relative order of other elements.
+ */
 void moveZeroes(int* nums, int numsSize) {
     int zeroIndex = 0;
 
