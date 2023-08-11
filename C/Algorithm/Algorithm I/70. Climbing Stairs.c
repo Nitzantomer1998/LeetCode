582dcb1b595d2c2653e7e44b40fc32e69629366e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-11 08:05:09 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/70. Climbing Stairs.c
+++ C/Algorithm/Algorithm I/70. Climbing Stairs.c
@@ -1,3 +1,17 @@
+/**
+ * Calculates the number of distinct ways to climb a staircase with 'n' steps.
+ *
+ * The 'climbStairs' function calculates the number of distinct ways to reach the
+ * top of a staircase with 'n' steps by taking one or two steps at a time. It uses
+ * a dynamic programming approach, iteratively updating the number of ways based on
+ * the previous steps taken. The final count of distinct ways is returned.
+ *
+ * Parameters:
+ * - n: The total number of steps in the staircase.
+ *
+ * Returns:
+ * The number of distinct ways to climb the staircase with 'n' steps.
+ */
 int climbStairs(int n) {
     int prevStairsOptions = 0;
     int currentStairsOptions = 1;
