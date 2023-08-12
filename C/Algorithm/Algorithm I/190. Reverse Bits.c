869531282ebb4ddcfd084d17a1409ad8d8bd131a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-12 20:04:01 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/190. Reverse Bits.c
+++ C/Algorithm/Algorithm I/190. Reverse Bits.c
@@ -19,13 +19,13 @@
  * reversal of the input integer.
  */
 uint32_t reverseBits(uint32_t n) {
-    int result = 0;
+    int reverseBitsValue = 0;
     
     for (int index = 31; index > 0; index--) {
-        result = (result + (n & 1)) * 2;
+        reverseBitsValue = (reverseBitsValue + (n & 1)) * 2;
         n >>= 1;
     }
 
-    result = (result + (n & 1));
-    return result;
+    reverseBitsValue = (reverseBitsValue + (n & 1));
+    return reverseBitsValue;
 }
