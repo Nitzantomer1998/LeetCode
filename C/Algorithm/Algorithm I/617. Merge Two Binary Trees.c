824918c5d2c6f5e6df925413eb2072658b840d6e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-13 10:43:44 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/617. Merge Two Binary Trees.c
+++ C/Algorithm/Algorithm I/617. Merge Two Binary Trees.c
@@ -1,3 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+/**
+ * Merges two binary trees.
+ *
+ * The 'mergeTrees' function takes two pointers to binary tree nodes 'root1' and 'root2'
+ * as input and merges them into a new binary tree. It creates a new binary tree node for
+ * the merged tree, calculates the values of the corresponding nodes in 'root1' and 'root2'
+ * (or 0 if a node is missing), and recursively merges the left and right subtrees. The
+ * merged tree is constructed by setting the node value and linking the left and right
+ * children to the merged subtrees.
+ *
+ * Parameters:
+ * - root1: A pointer to the root node of the first binary tree.
+ * - root2: A pointer to the root node of the second binary tree.
+ *
+ * Returns:
+ * A pointer to the root node of the merged binary tree.
+ */
 struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2) {
     struct TreeNode* mergedTree = (struct TreeNode*) malloc (sizeof(struct TreeNode));
 
