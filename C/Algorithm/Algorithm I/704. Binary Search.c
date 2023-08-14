6d3569a8dda0e6d97c40f50da35b0a08693d324a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-14 08:15:50 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/704. Binary Search.c
+++ C/Algorithm/Algorithm I/704. Binary Search.c
@@ -1,3 +1,20 @@
+/**
+ * Searches for a target value in a sorted array using binary search.
+ *
+ * The 'search' function performs a binary search on a sorted integer array 'nums'
+ * to find the index of the target value. It compares the target value with the
+ * middle element of the array and updates the search range accordingly until the
+ * target is found or the search range is exhausted. If the target is found, the
+ * index of the target value is returned; otherwise, -1 is returned.
+ *
+ * Parameters:
+ * - nums: A sorted array of integers.
+ * - numsSize: The number of elements in the array.
+ * - target: The value to search for in the array.
+ *
+ * Returns:
+ * The index of the target value in the array, or -1 if the target is not found.
+ */
 int search(int* nums, int numsSize, int target) {
     int leftPointer = 0;
     int rightPointer = numsSize - 1;
