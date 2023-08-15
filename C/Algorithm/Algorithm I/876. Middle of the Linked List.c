cc79376df1237fac73b6b3bf7e6f15c909ee856c
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-15 13:05:22 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/876. Middle of the Linked List.c
+++ C/Algorithm/Algorithm I/876. Middle of the Linked List.c
@@ -1,3 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+/**
+ * Finds the middle node of a singly-linked list.
+ *
+ * The 'middleNode' function takes a singly-linked list 'head' as input and uses two pointers,
+ * 'middleListNode' and 'traverseListNode', to traverse the list. The 'middleListNode' pointer
+ * moves at half the pace of the 'traverseListNode' pointer. When the 'traverseListNode' reaches
+ * the end of the list, the 'middleListNode' points to the middle node.
+ *
+ * Parameters:
+ * - head: A pointer to the head node of the singly-linked list.
+ *
+ * Returns:
+ * A pointer to the middle node of the singly-linked list.
+ */
 struct ListNode* middleNode(struct ListNode* head) {
     struct ListNode* middleListNode = head;
     struct ListNode* traverseListNode = head;
