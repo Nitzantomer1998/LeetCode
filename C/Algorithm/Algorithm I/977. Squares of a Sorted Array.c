0152c251986c76c782de6413fa186dff33d5a434
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-16 10:23:12 +0300

    Adding Notes

--- C/Algorithm/Algorithm I/977. Squares of a Sorted Array.c
+++ C/Algorithm/Algorithm I/977. Squares of a Sorted Array.c
@@ -1,3 +1,16 @@
+/**
+ * Merges two subarrays of the input array in sorted order.
+ *
+ * The 'merge' function takes an input array, and two indices 'leftPointer' and 'rightPointer'
+ * that define the subarrays to be merged. It creates temporary arrays for the left and right
+ * subarrays, then merges them back into the input array in sorted order.
+ *
+ * Parameters:
+ * - array: The input array containing two subarrays to be merged.
+ * - leftPointer: The index of the start of the left subarray.
+ * - middlePointer: The index of the end of the left subarray and the start of the right subarray.
+ * - rightPointer: The index of the end of the right subarray.
+ */
 void merge(int* array, int leftPointer, int middlePointer, int rightPointer) {
     int leftSize = middlePointer - leftPointer + 1;
     int rightSize = rightPointer - middlePointer;
@@ -45,6 +58,17 @@ void merge(int* array, int leftPointer, int middlePointer, int rightPointer) {
     free(rightArray);
 }
 
+/**
+ * Applies the merge sort algorithm to sort an input array.
+ *
+ * The 'mergeSort' function implements the merge sort algorithm to sort the elements of an input array.
+ * It recursively divides the array into subarrays, sorts each subarray, and then merges them together.
+ *
+ * Parameters:
+ * - array: The input array to be sorted.
+ * - leftPointer: The index of the start of the subarray to be sorted.
+ * - rightPointer: The index of the end of the subarray to be sorted.
+ */
 void mergeSort(int* array, int leftPointer, int rightPointer) {
     if (leftPointer < rightPointer) {
         int middlePointer = (leftPointer + rightPointer) / 2;
@@ -56,6 +80,21 @@ void mergeSort(int* array, int leftPointer, int rightPointer) {
     }
 }
 
+/**
+ * Generates an array of sorted squares from the input array of integers.
+ *
+ * The 'sortedSquares' function takes an array of integers 'nums', squares each element, and then
+ * applies the merge sort algorithm to sort the squared values in ascending order. The sorted
+ * squared values are returned in a new dynamically allocated array.
+ *
+ * Parameters:
+ * - nums: The input array of integers.
+ * - numsSize: The size of the input array.
+ * - returnSize: A pointer to the variable that will store the size of the sorted array.
+ *
+ * Returns:
+ * A dynamically allocated array of integers representing the sorted squares of the input array.
+ */
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
     int* squareSortedArray = (int*) malloc (sizeof(int) * numsSize);
     *returnSize = numsSize;
