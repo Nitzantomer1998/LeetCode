0021b9917cf11d3380357aad47c6eb83517a94e6
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-20 18:33:21 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/120. Triangle.ts
+++ TypeScript/Algorithm/Algorithm I/120. Triangle.ts
@@ -1,7 +1,19 @@
+/**
+ * Returns the minimum value between two numbers.
+ * @param valueA - The first number.
+ * @param valueB - The second number.
+ * @returns The minimum value between `valueA` and `valueB`.
+ */
 function minValue(valueA: number, valueB: number): number { 
     return valueA < valueB ? valueA : valueB;
 }
 
+/**
+ * Finds the minimum total path sum from the top to the bottom of a triangle.
+ * Each step, you may move to adjacent numbers on the row below.
+ * @param triangle - A two-dimensional array representing the triangle of numbers.
+ * @returns The minimum total path sum.
+ */
 function minimumTotal(triangle: number[][]): number {
     const triangleHeight: number = triangle.length;
 
