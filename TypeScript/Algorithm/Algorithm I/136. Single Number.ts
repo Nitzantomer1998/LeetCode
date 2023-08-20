3962e1556652828790b7030154929923f629406e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-20 18:34:12 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/136. Single Number.ts
+++ TypeScript/Algorithm/Algorithm I/136. Single Number.ts
@@ -1,3 +1,8 @@
+/**
+ * Finds the single number in an array where all other elements appear twice.
+ * @param nums - An array of numbers where each element appears twice except for one element.
+ * @returns The number that appears only once in the array.
+ */
 function singleNumber(nums: number[]): number {
     const numsLength: number = nums.length;
 
