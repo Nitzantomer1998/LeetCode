7b4032a0ee548e7d1bf1f40cf242938dde5e09cb
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-20 18:32:22 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/116. Populating Next Right Pointers in Each Node.ts
+++ TypeScript/Algorithm/Algorithm I/116. Populating Next Right Pointers in Each Node.ts
@@ -1,3 +1,24 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     left: Node | null
+ *     right: Node | null
+ *     next: Node | null
+ *     constructor(val?: number, left?: Node, right?: Node, next?: Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ * Updates the 'next' pointers of nodes in a binary tree according to specific rules.
+ * @param {Node | null} leftNode - The left node.
+ * @param {Node | null} rightNode - The right node.
+ */
 function updateNextPointer(leftNode: Node | null, rightNode: Node | null): void {
     if (leftNode && rightNode) {
         leftNode.next = rightNode;
@@ -5,6 +26,11 @@ function updateNextPointer(leftNode: Node | null, rightNode: Node | null): void 
     }
 }
 
+/**
+ * Connects nodes in a binary tree to their adjacent nodes in the same level.
+ * @param {Node | null} root - The root node of the binary tree.
+ * @returns {Node | null} The root node of the modified binary tree.
+ */
 function connect(root: Node | null): Node | null {
     if (root) {
         updateNextPointer(root.left, root.right);
