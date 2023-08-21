e8f049b99bacb7d45db6c149942afef47e39bf6d
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-21 08:21:47 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/19. Remove Nth Node From End of List.ts
+++ TypeScript/Algorithm/Algorithm I/19. Remove Nth Node From End of List.ts
@@ -1,3 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * @class ListNode
+ * @property {number} val - The value of the node.
+ * @property {ListNode | null} next - The reference to the next node.
+ * @constructor Create a ListNode.
+ * @param {number} [val] - The value of the node.
+ * @param {ListNode | null} [next] - The reference to the next node.
+ 
+* class ListNode {
+*     val: number;
+*     next: ListNode | null;
+*     constructor(val?: number, next?: ListNode | null) {
+*         this.val = (val === undefined ? 0 : val);
+*         this.next = (next === undefined ? null : next);
+*     }
+* }
+*/
+
+/**
+ * Returns the length of a given singly-linked list.
+ * @param {ListNode | null} head - The head node of the linked list.
+ * @returns {number} The length of the linked list.
+ */
 function getListNodeLength(head: ListNode | null): number {
     let listNodeLength: number = 0;
     
@@ -9,6 +33,12 @@ function getListNodeLength(head: ListNode | null): number {
     return listNodeLength;
 }
 
+/**
+ * Removes the nth node from the end of a singly-linked list.
+ * @param {ListNode | null} head - The head node of the linked list.
+ * @param {number} n - The position of the node to be removed from the end.
+ * @returns {ListNode | null} The head of the linked list after removal.
+ */
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
     let traverse: ListNode | null = head;
     let listNodeLength = getListNodeLength(head);
