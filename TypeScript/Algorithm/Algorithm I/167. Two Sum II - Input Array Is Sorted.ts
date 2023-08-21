da87963134f2734efc80dc82ebc4435b719b1161
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-21 08:25:06 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/167. Two Sum II - Input Array Is Sorted.ts
+++ TypeScript/Algorithm/Algorithm I/167. Two Sum II - Input Array Is Sorted.ts
@@ -1,16 +1,27 @@
 /**
- * Reverses the bits of a given integer.
- * @param {number} n - The integer whose bits are to be reversed.
- * @returns {number} The integer with reversed bits.
+ * Finds a pair of numbers in a sorted array that add up to the given target.
+ * @param numbers - A sorted array of numbers.
+ * @param target - The target sum to be achieved.
+ * @returns An array containing the indices of the two numbers that add up to the target.
  */
-function reverseBits(n: number): number {
-    let reverseN: number = 0;
+function twoSum(numbers: number[], target: number): number[] {
+    const numbersLength: number = numbers.length;
 
-    for (let _: number = 0; _ < 31; _++) {
-        reverseN = (reverseN + (n & 1)) * 2;
-        n >>= 1;
+    let leftPointer: number = 0;
+    let rightPointer: number = numbersLength - 1;
+
+    while (leftPointer < rightPointer) {
+        const currentSum: number = numbers[leftPointer] + numbers[rightPointer];
+
+        if (currentSum === target)
+            return [leftPointer + 1, rightPointer + 1];
+
+        else if (currentSum > target)
+            rightPointer--;
+    
+        else
+            leftPointer++;
     }
 
-    reverseN = (reverseN + (n & 1));
-    return reverseN;
+    return [leftPointer + 1, rightPointer + 1];
 };
