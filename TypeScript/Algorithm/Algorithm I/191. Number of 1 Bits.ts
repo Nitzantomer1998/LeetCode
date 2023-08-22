7fed4b9768c653c3cbd0e8085a68566dc04c51ca
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-22 14:52:12 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/191. Number of 1 Bits.ts
+++ TypeScript/Algorithm/Algorithm I/191. Number of 1 Bits.ts
@@ -1,3 +1,8 @@
+/**
+ * Calculates the Hamming weight (number of set bits) in a given integer.
+ * @param {number} n - The integer for which to calculate the Hamming weight.
+ * @returns {number} The count of set bits in the integer.
+ */
 function hammingWeight(n: number): number {
     let oneBitCounter: number = 0;
 
