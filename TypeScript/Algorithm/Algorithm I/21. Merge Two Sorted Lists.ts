6d614013b894bca169c063e5cd5dbc3c5abbd095
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-22 14:49:41 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/21. Merge Two Sorted Lists.ts
+++ TypeScript/Algorithm/Algorithm I/21. Merge Two Sorted Lists.ts
@@ -1,3 +1,8 @@
+/**
+ * Checks if a given number is a power of two.
+ * @param {number} n - The number to be checked.
+ * @returns {boolean} Returns true if the number is a power of two, otherwise false.
+ */
 function isPowerOfTwo(n: number): boolean {
     return (n & (n - 1)) === 0 && n > 0;
 };
