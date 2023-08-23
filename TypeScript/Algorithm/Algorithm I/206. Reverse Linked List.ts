1adc8751428b9bea5409ff87ed16b55c1c5a5b67
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-23 10:51:11 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/206. Reverse Linked List.ts
+++ TypeScript/Algorithm/Algorithm I/206. Reverse Linked List.ts
@@ -1,3 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * @class ListNode
+ * @property {number} val - The value of the node.
+ * @property {ListNode | null} next - The reference to the next node.
+ * @constructor Create a ListNode.
+ * @param {number} [val] - The value of the node.
+ * @param {ListNode | null} [next] - The reference to the next node.
+ * 
+ * class ListNode {
+ *     val: number;
+ *     next: ListNode | null;
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val === undefined ? 0 : val);
+ *         this.next = (next === undefined ? null : next);
+ *     }
+ * }
+*/
+
+/**
+ * Reverses a singly-linked list.
+ * @param {ListNode | null} head - The head node of the linked list to be reversed.
+ * @returns {ListNode | null} The head of the reversed linked list.
+ */
 function reverseList(head: ListNode | null): ListNode | null {
     let currentNode: ListNode | null = head;
     let previousNode: ListNode | null = null;
