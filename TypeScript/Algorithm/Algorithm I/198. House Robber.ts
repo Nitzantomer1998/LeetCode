314c52b9df44ffa7d474245bd8d1b2684503ccda
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-23 10:50:11 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/198. House Robber.ts
+++ TypeScript/Algorithm/Algorithm I/198. House Robber.ts
@@ -1,7 +1,18 @@
+/**
+ * Returns the maximum of two numbers.
+ * @param {number} valueA - The first number.
+ * @param {number} valueB - The second number.
+ * @returns {number} The larger of the two input numbers.
+ */
 function maxValue(valueA: number, valueB: number): number {
     return valueA > valueB ? valueA : valueB;
 }
 
+/**
+ * Determines the maximum amount that can be robbed from a row of houses, considering certain constraints.
+ * @param {number[]} nums - An array of integers representing the amounts in each house.
+ * @returns {number} The maximum amount that can be robbed without robbing adjacent houses.
+ */
 function rob(nums: number[]): number {
     const numsLength: number = nums.length;
 
