81c350b5493f29e052f87f3632ba9ef284963d4a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-24 11:45:15 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/3. Longest Substring Without Repeating Characters.ts
+++ TypeScript/Algorithm/Algorithm I/3. Longest Substring Without Repeating Characters.ts
@@ -1,7 +1,18 @@
+/**
+ * Returns the maximum of two numbers.
+ * @param {number} valueA - The first number.
+ * @param {number} valueB - The second number.
+ * @returns {number} The larger of the two input numbers.
+ */
 function maxValue(valueA: number, valueB: number): number {
     return valueA > valueB ? valueA : valueB;
 }
 
+/**
+ * Finds the length of the longest substring without repeating characters.
+ * @param {string} s - The input string.
+ * @returns {number} The length of the longest substring without repeating characters.
+ */
 function lengthOfLongestSubstring(s: string): number {
     const stringLength: number = s.length;
     let longestSubstring: number = 0;
