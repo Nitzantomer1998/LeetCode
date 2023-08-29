0ff7b2869b9543bcebb0465bf46649d0a27905c7
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-29 09:05:11 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/617. Merge Two Binary Trees.ts
+++ TypeScript/Algorithm/Algorithm I/617. Merge Two Binary Trees.ts
@@ -1,3 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * @class TreeNode
+ * @property {number} val - The value of the node.
+ * @property {TreeNode | null} left - The left child node.
+ * @property {TreeNode | null} right - The right child node.
+ * @constructor Create a TreeNode.
+ * @param {number} [val] - The value of the node.
+ * @param {TreeNode | null} [left] - The left child node.
+ * @param {TreeNode | null} [right] - The right child node.
+ * 
+ * class TreeNode {
+ *     val: number;
+ *     left: TreeNode | null;
+ *     right: TreeNode | null;
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val === undefined ? 0 : val);
+ *         this.left = (left === undefined ? null : left);
+ *         this.right = (right === undefined ? null : right);
+ *     }
+ * }
+*/
+
+/**
+ * Merges two binary trees by summing their node values.
+ * @param {TreeNode | null} root1 - The root node of the first tree.
+ * @param {TreeNode | null} root2 - The root node of the second tree.
+ * @returns {TreeNode | null} The root node of the merged binary tree.
+ */
 function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | null {
     if (root1 === null && root2 === null)
         return null;
