d7bba892c7536e0f78c573cfb8afdded8058f645
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-29 09:04:25 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/567. Permutation in String.ts
+++ TypeScript/Algorithm/Algorithm I/567. Permutation in String.ts
@@ -1,3 +1,9 @@
+/**
+ * Checks if a substring of s2 contains a permutation of s1.
+ * @param {string} s1 - The first string to be checked for permutation.
+ * @param {string} s2 - The second string to be checked for the presence of a permutation of s1.
+ * @returns {boolean} Returns true if s2 contains a permutation of s1, otherwise false.
+ */
 function checkInclusion(s1: string, s2: string): boolean {
     const sOneLength: number = s1.length;
     const sTwoLength: number = s2.length;
