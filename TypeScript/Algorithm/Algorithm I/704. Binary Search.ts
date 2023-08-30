ee7bae0f85e2988cc42abccd20a906842c09637e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-30 11:36:21 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/704. Binary Search.ts
+++ TypeScript/Algorithm/Algorithm I/704. Binary Search.ts
@@ -1,22 +1,28 @@
-function search(nums: number[], target: number): number {
-    const numsLength: number = nums.length;
-    
-    let leftPointer: number = 0;
-    let rightPointer: number = numsLength - 1;
+    /**
+     * Searches for a target value in a sorted array using binary search.
+     * @param {number[]} nums - The sorted array to search in.
+     * @param {number} target - The target value to search for.
+     * @returns {number} The index of the target value in the array, or -1 if not found.
+     */
+    function search(nums: number[], target: number): number {
+        const numsLength: number = nums.length;
+        
+        let leftPointer: number = 0;
+        let rightPointer: number = numsLength - 1;
 
-    while (leftPointer <= rightPointer) {
-        const middlePointer: number = Math.floor((leftPointer + rightPointer) / 2);
-        const middleValue: number = nums[middlePointer];
+        while (leftPointer <= rightPointer) {
+            const middlePointer: number = Math.floor((leftPointer + rightPointer) / 2);
+            const middleValue: number = nums[middlePointer];
 
-        if (middleValue === target)
-            return middlePointer;
+            if (middleValue === target)
+                return middlePointer;
 
-        else if (middleValue > target)
-            rightPointer = middlePointer - 1;
+            else if (middleValue > target)
+                rightPointer = middlePointer - 1;
 
-        else
-            leftPointer = middlePointer + 1;
-    }
+            else
+                leftPointer = middlePointer + 1;
+        }
 
-    return -1;
-};
+        return -1;
+    };
