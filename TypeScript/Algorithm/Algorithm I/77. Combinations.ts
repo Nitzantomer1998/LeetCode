d68b2ac604efe1d14c599a7311e1328c26a60cf1
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-31 08:28:01 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/77. Combinations.ts
+++ TypeScript/Algorithm/Algorithm I/77. Combinations.ts
@@ -1,6 +1,17 @@
+/**
+ * Generates all possible combinations of k numbers from 1 to n.
+ * @param {number} n - The range of numbers from 1 to n.
+ * @param {number} k - The number of elements in each combination.
+ * @returns {number[][]} An array of arrays containing all combinations.
+ */
 function combine(n: number, k: number): number[][] {
   const combinationArray: number[][] = [];
 
+  /**
+   * Performs depth-first search (DFS) to generate combinations.
+   * @param {number[]} currentCombination - The current combination being formed.
+   * @param {number} startIndex - The index to start considering numbers from.
+   */
   function dfs(currentCombination: number[], startIndex: number) {
     if (currentCombination.length === k) {
       combinationArray.push(currentCombination);
