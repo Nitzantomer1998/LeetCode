1d2fb0d04545d06fabb3b09140971de504767ee1
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-31 08:30:40 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/876. Middle of the Linked List.ts
+++ TypeScript/Algorithm/Algorithm I/876. Middle of the Linked List.ts
@@ -1,3 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * @class ListNode
+ * @property {number} val - The value of the node.
+ * @property {ListNode | null} next - The next node in the linked list.
+ * @constructor Create a ListNode.
+ * @param {number} [val] - The value of the node.
+ * @param {ListNode | null} [next] - The next node in the linked list.
+ *
+ * class ListNode {
+ *     val: number;
+ *     next: ListNode | null;
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val === undefined ? 0 : val);
+ *         this.next = (next === undefined ? null : next);
+ *     }
+ * }
+ */
+
+/**
+ * Finds the middle node of a linked list.
+ * @param {ListNode | null} head - The head node of the linked list.
+ * @returns {ListNode | null} The middle node of the linked list.
+ */
 function middleNode(head: ListNode | null): ListNode | null {
   let fastTraverse: ListNode | null = head;
   let slowTraverse: ListNode | null = head;
