e93f10877ef385b7c0015e72a4ae4d291f9e1f5f
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-08-31 08:29:19 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/784. Letter Case Permutation.ts
+++ TypeScript/Algorithm/Algorithm I/784. Letter Case Permutation.ts
@@ -1,6 +1,16 @@
+/**
+ * Generates all possible letter case permutations of the given string.
+ * @param {string} s - The input string containing letters and digits.
+ * @returns {string[]} An array of strings containing all letter case permutations.
+ */
 function letterCasePermutation(s: string): string[] {
   const combinationArray: string[] = [];
 
+  /**
+   * Performs depth-first search (DFS) to generate letter case permutations.
+   * @param {string} currentCombination - The current combination being formed.
+   * @param {number} currentIndex - The index of the current character in the string.
+   */
   function dfs(currentCombination: string, currentIndex: number) {
     if (currentIndex === s.length) {
       combinationArray.push(currentCombination);
