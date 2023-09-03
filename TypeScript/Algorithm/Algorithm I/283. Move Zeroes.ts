7f2fad29595a0ed09f665a0bd162138837ea0337
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 07:53:55 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/283. Move Zeroes.ts
+++ TypeScript/Algorithm/Algorithm I/283. Move Zeroes.ts
@@ -4,16 +4,17 @@
  * @returns {void}
  */
 function moveZeroes(nums: number[]): void {
-    const numsLength: number = nums.length;
-    let nonZeroIndex: number = 0;
+  const LENGTH: number = nums.length;
+  let nonZeroIndex: number = 0;
 
-    for (let index: number = 0; index < numsLength; index++) {
-        if (nums[index]) {
-            if (index !== nonZeroIndex) {
-                nums[nonZeroIndex] = nums[index];
-                nums[index] = 0;
-            }
-            nonZeroIndex++;
-        }
+  for (let index: number = 0; index < LENGTH; index++) {
+    if (nums[index]) {
+      if (index !== nonZeroIndex) {
+        nums[nonZeroIndex] = nums[index];
+        nums[index] = 0;
+      }
+
+      nonZeroIndex++;
     }
+  }
 }
