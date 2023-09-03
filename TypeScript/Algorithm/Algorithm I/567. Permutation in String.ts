019657baf2f7bd77eaa1f01a31688015a0879ea4
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 08:09:22 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/567. Permutation in String.ts
+++ TypeScript/Algorithm/Algorithm I/567. Permutation in String.ts
@@ -5,27 +5,27 @@
  * @returns {boolean} Returns true if s2 contains a permutation of s1, otherwise false.
  */
 function checkInclusion(s1: string, s2: string): boolean {
-    const sOneLength: number = s1.length;
-    const sTwoLength: number = s2.length;
+  const S1_LENGTH: number = s1.length;
+  const S2_LENGTH: number = s2.length;
 
-    const stringOneContent: number[] = new Array(26).fill(0);
-    const stringTwoContent: number[] = new Array(26).fill(0);
+  const stringOneContent: number[] = new Array(26).fill(0);
+  const stringTwoContent: number[] = new Array(26).fill(0);
 
-    for (let index: number = 0; index < sOneLength; index++) {
-        stringOneContent[s1.charCodeAt(index) - 97]++;
-        stringTwoContent[s2.charCodeAt(index) - 97]++;
-    }
+  for (let index: number = 0; index < S1_LENGTH; index++) {
+    stringOneContent[s1.charCodeAt(index) - 97]++;
+    stringTwoContent[s2.charCodeAt(index) - 97]++;
+  }
 
-    for (let index: number = sOneLength; index < sTwoLength; index++) {
-        if (stringOneContent.every((count, i) => count === stringTwoContent[i]))
-            return true;
+  for (let index: number = S1_LENGTH; index < S2_LENGTH; index++) {
+    if (stringOneContent.every((count, i) => count === stringTwoContent[i]))
+      return true;
 
-        const charToAdd: number = s2.charCodeAt(index) - 97;
-        const charToRemove: number = s2.charCodeAt(index - sOneLength) - 97;
+    const charToAdd: number = s2.charCodeAt(index) - 97;
+    const charToRemove: number = s2.charCodeAt(index - S1_LENGTH) - 97;
 
-        stringTwoContent[charToAdd]++;
-        stringTwoContent[charToRemove]--;
-    }
+    stringTwoContent[charToAdd]++;
+    stringTwoContent[charToRemove]--;
+  }
 
-    return stringOneContent.every((count, i) => count === stringTwoContent[i]);
+  return stringOneContent.every((count, i) => count === stringTwoContent[i]);
 }
