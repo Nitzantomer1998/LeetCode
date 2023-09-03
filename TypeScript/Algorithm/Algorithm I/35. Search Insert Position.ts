e2892e06c9eb4ad79857290a2a60c5e57ad9c2a6
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 08:01:03 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/35. Search Insert Position.ts
+++ TypeScript/Algorithm/Algorithm I/35. Search Insert Position.ts
@@ -5,24 +5,19 @@
  * @returns {number} The index where the target value should be inserted.
  */
 function searchInsert(nums: number[], target: number): number {
-    const numsLength: number = nums.length;
+  const LENGTH: number = nums.length;
 
-    let leftPointer: number = 0;
-    let rightPointer: number = numsLength;
+  let leftPointer: number = 0;
+  let rightPointer: number = LENGTH;
 
-    while (leftPointer <= rightPointer) {
-        const middlePointer: number = Math.floor((leftPointer + rightPointer) / 2);
-        const middleValue: number = nums[middlePointer];
+  while (leftPointer <= rightPointer) {
+    const middlePointer: number = Math.floor((leftPointer + rightPointer) / 2);
+    const middleValue: number = nums[middlePointer];
 
-        if (middleValue === target)
-            return middlePointer;
+    if (middleValue === target) return middlePointer;
+    else if (middleValue < target) leftPointer = middlePointer + 1;
+    else rightPointer = middlePointer - 1;
+  }
 
-        else if (middleValue < target)
-            leftPointer = middlePointer + 1;
-
-        else
-            rightPointer = middlePointer - 1;
-    }
-
-    return rightPointer + 1;
-};
+  return rightPointer + 1;
+}
