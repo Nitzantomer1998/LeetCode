6882e29a3df111b5cabd37b09f883a23d8a5e900
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 08:13:57 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/733. Flood Fill.ts
+++ TypeScript/Algorithm/Algorithm I/733. Flood Fill.ts
@@ -7,27 +7,26 @@
  * @returns {number[][]} The modified image with the updated color.
  */
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
-    const ROWS: number = image.length;
-    const COLS: number = image[0].length;
+  const ROWS: number = image.length;
+  const COLS: number = image[0].length;
 
-    const oldColor: number = image[sr][sc];
+  const oldColor: number = image[sr][sc];
 
-    if (oldColor === color)
-        return image;
+  if (oldColor === color) return image;
 
-    function dfs(row: number, col: number): void {
-        if (row < 0 || col < 0 || row >= ROWS || col >= COLS || image[row][col] !== oldColor)
-            return;
+  function dfs(row: number, col: number): void {
+    if (row < 0 || col < 0 || row >= ROWS || col >= COLS || image[row][col] !== oldColor)
+      return;
 
-        image[row][col] = color;
+    image[row][col] = color;
 
-        dfs(row - 1, col); // Up
-        dfs(row + 1, col); // Down
-        dfs(row, col - 1); // Left
-        dfs(row, col + 1); // Right
-    }
+    dfs(row - 1, col); // Up
+    dfs(row + 1, col); // Down
+    dfs(row, col - 1); // Left
+    dfs(row, col + 1); // Right
+  }
 
-    dfs(sr, sc);
+  dfs(sr, sc);
 
-    return image;
-};
+  return image;
+}
