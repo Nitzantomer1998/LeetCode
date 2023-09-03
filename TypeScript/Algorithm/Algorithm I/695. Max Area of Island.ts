c053339c2284d93817dcfcb76a8c166dd0e95e1c
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 08:11:56 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/695. Max Area of Island.ts
+++ TypeScript/Algorithm/Algorithm I/695. Max Area of Island.ts
@@ -6,20 +6,20 @@
  * @returns {number} The size of the island.
  */
 function dfs(grid: number[][], row: number, column: number): number {
-    if (row >= 0 && column >= 0 && row < grid.length && column < grid[0].length) {
-        if (grid[row][column]) {
-            grid[row][column] = 0;
+  if (row >= 0 && column >= 0 && row < grid.length && column < grid[0].length) {
+    if (grid[row][column]) {
+      grid[row][column] = 0;
 
-            const upCell: number = dfs(grid, row - 1, column);
-            const downCell: number = dfs(grid, row + 1, column);
-            const leftCell: number = dfs(grid, row, column - 1);
-            const rightCell: number = dfs(grid, row, column + 1);
+      const upCell: number = dfs(grid, row - 1, column);
+      const downCell: number = dfs(grid, row + 1, column);
+      const leftCell: number = dfs(grid, row, column - 1);
+      const rightCell: number = dfs(grid, row, column + 1);
 
-            return upCell + downCell + leftCell + rightCell + 1;
-        }
+      return upCell + downCell + leftCell + rightCell + 1;
     }
+  }
 
-    return 0;
+  return 0;
 }
 
 /**
@@ -28,15 +28,15 @@ function dfs(grid: number[][], row: number, column: number): number {
  * @returns {number} The maximum area of an island in the grid.
  */
 function maxAreaOfIsland(grid: number[][]): number {
-    let maxIslandCounter: number = 0;
-    
-    const rowSize: number = grid.length;
-    const columnSize: number = grid[0].length;
+  const ROWS: number = grid.length;
+  const COLUMNS: number = grid[0].length;
 
-    for (let row: number = 0; row < rowSize; row++)
-        for (let column: number = 0; column < columnSize; column++)
-            if (grid[row][column])
-                maxIslandCounter = Math.max(maxIslandCounter, dfs(grid, row, column));
+  let maxIslandCounter: number = 0;
 
-    return maxIslandCounter;
-};
+  for (let row: number = 0; row < ROWS; row++)
+    for (let column: number = 0; column < COLUMNS; column++)
+      if (grid[row][column])
+        maxIslandCounter = Math.max(maxIslandCounter, dfs(grid, row, column));
+
+  return maxIslandCounter;
+}
