87599ceba96403fb0a321172d8c00befb20b67b7
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 07:40:23 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/189. Rotate Array.ts
+++ TypeScript/Algorithm/Algorithm I/189. Rotate Array.ts
@@ -1,32 +1,32 @@
 /**
+ * Rotates a portion of an array between the start and end indices.
+ * @param nums - The array of numbers to be rotated.
+ * @param startIndex - The starting index of the rotation.
+ * @param endIndex - The ending index of the rotation.
+ */
+function rotateArray(nums: number[], startIndex: number, endIndex: number): void {
+  while (startIndex < endIndex) {
+    const tempValue: number = nums[startIndex];
+
+    nums[startIndex] = nums[endIndex];
+    nums[endIndex] = tempValue;
+
+    startIndex++;
+    endIndex--;
+  }
+}
+
+/**
  * Rotates the elements of an array to the right by a specified number of steps.
  * @param nums - The array of numbers to be rotated.
  * @param k - The number of steps to rotate the array.
  */
 function rotate(nums: number[], k: number): void {
-    const numsLength: number = nums.length;
-    
-    k = k % numsLength;
-    
-    rotateArray(nums, 0, numsLength - 1);
-    rotateArray(nums, 0, k - 1);
-    rotateArray(nums, k, numsLength - 1);
-}
+  const LENGTH: number = nums.length;
 
-/**
- * Rotates a portion of an array between the start and end indices.
- * @param nums - The array of numbers to be rotated.
- * @param start - The starting index of the rotation.
- * @param end - The ending index of the rotation.
- */
-function rotateArray(nums: number[], start: number, end: number): void {
-    while (start < end) {
-        const temp: number = nums[start];
+  k = k % LENGTH;
 
-        nums[start] = nums[end];
-        nums[end] = temp;
-        
-        start++;
-        end--;
-    }
+  rotateArray(nums, 0, LENGTH - 1);
+  rotateArray(nums, 0, k - 1);
+  rotateArray(nums, k, LENGTH - 1);
 }
