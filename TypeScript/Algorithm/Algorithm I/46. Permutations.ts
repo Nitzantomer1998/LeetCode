c389d34182cc8d3937188cd6f4101f9a9b3abeed
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 08:01:55 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/46. Permutations.ts
+++ TypeScript/Algorithm/Algorithm I/46. Permutations.ts
@@ -4,21 +4,22 @@
  * @returns {number[][]} An array of arrays containing all possible permutations.
  */
 function permute(nums: number[]): number[][] {
-    const numsLength: number = nums.length;
-    const permutatedArray: number[][] = [[nums[0]]];
+  const LENGTH: number = nums.length;
+  const permutatedArray: number[][] = [[nums[0]]];
 
-    for (let index: number = 1; index < numsLength; index++) {
-        permutatedArray.forEach(item => {
-            const currentLength: number = item.push(nums[index]);
+  for (let index: number = 1; index < LENGTH; index++) {
+    permutatedArray.forEach((item) => {
+      const currentLength: number = item.push(nums[index]);
 
-            for (let j = currentLength - 1; j > 0; j--) {
-                item = item.slice();
-                item[j] = item[j - 1];
-                item[j - 1] = nums[index];
-                permutatedArray.push(item);
-            }
-        });
-    }
-    
-    return permutatedArray;
-};
+      for (let j = currentLength - 1; j > 0; j--) {
+        item = item.slice();
+        item[j] = item[j - 1];
+        item[j - 1] = nums[index];
+        
+        permutatedArray.push(item);
+      }
+    });
+  }
+
+  return permutatedArray;
+}
