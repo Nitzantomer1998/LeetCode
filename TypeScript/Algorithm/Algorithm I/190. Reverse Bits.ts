88f1d84e90169926153981b685258154eb2659d5
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 07:45:34 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/190. Reverse Bits.ts
+++ TypeScript/Algorithm/Algorithm I/190. Reverse Bits.ts
@@ -4,13 +4,13 @@
  * @returns {number} The integer with reversed bits.
  */
 function reverseBits(n: number): number {
-    let reverseN: number = 0;
+  let reverseN: number = 0;
 
-    for (let _: number = 0; _ < 31; _++) {
-        reverseN = (reverseN + (n & 1)) * 2;
-        n >>= 1;
-    }
+  for (let _: number = 0; _ < 31; _++) {
+    reverseN = (reverseN + (n & 1)) * 2;
+    n >>= 1;
+  }
 
-    reverseN = (reverseN + (n & 1));
-    return reverseN;
-};
+  reverseN = reverseN + (n & 1);
+  return reverseN;
+}
