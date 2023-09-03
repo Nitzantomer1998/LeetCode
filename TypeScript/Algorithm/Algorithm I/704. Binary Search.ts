9e6d7eaa95634aaada4cc96b296093bcf57e83df
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 08:12:47 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/704. Binary Search.ts
+++ TypeScript/Algorithm/Algorithm I/704. Binary Search.ts
@@ -1,28 +1,23 @@
-    /**
-     * Searches for a target value in a sorted array using binary search.
-     * @param {number[]} nums - The sorted array to search in.
-     * @param {number} target - The target value to search for.
-     * @returns {number} The index of the target value in the array, or -1 if not found.
-     */
-    function search(nums: number[], target: number): number {
-        const numsLength: number = nums.length;
-        
-        let leftPointer: number = 0;
-        let rightPointer: number = numsLength - 1;
+/**
+ * Searches for a target value in a sorted array using binary search.
+ * @param {number[]} nums - The sorted array to search in.
+ * @param {number} target - The target value to search for.
+ * @returns {number} The index of the target value in the array, or -1 if not found.
+ */
+function search(nums: number[], target: number): number {
+  const LENGTH: number = nums.length;
 
-        while (leftPointer <= rightPointer) {
-            const middlePointer: number = Math.floor((leftPointer + rightPointer) / 2);
-            const middleValue: number = nums[middlePointer];
+  let leftPointer: number = 0;
+  let rightPointer: number = LENGTH - 1;
 
-            if (middleValue === target)
-                return middlePointer;
+  while (leftPointer <= rightPointer) {
+    const middlePointer: number = Math.floor((leftPointer + rightPointer) / 2);
+    const middleValue: number = nums[middlePointer];
 
-            else if (middleValue > target)
-                rightPointer = middlePointer - 1;
+    if (middleValue === target) return middlePointer;
+    else if (middleValue > target) rightPointer = middlePointer - 1;
+    else leftPointer = middlePointer + 1;
+  }
 
-            else
-                leftPointer = middlePointer + 1;
-        }
-
-        return -1;
-    };
+  return -1;
+}
