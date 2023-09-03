7254f09c157dd1353dce0c76b5f13991f04b478b
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-03 07:36:09 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm I/120. Triangle.ts
+++ TypeScript/Algorithm/Algorithm I/120. Triangle.ts
@@ -4,8 +4,8 @@
  * @param valueB - The second number.
  * @returns The minimum value between `valueA` and `valueB`.
  */
-function minValue(valueA: number, valueB: number): number { 
-    return valueA < valueB ? valueA : valueB;
+function minValue(valueA: number, valueB: number): number {
+  return valueA < valueB ? valueA : valueB;
 }
 
 /**
@@ -15,18 +15,18 @@ function minValue(valueA: number, valueB: number): number {
  * @returns The minimum total path sum.
  */
 function minimumTotal(triangle: number[][]): number {
-    const triangleHeight: number = triangle.length;
+  const ROWS: number = triangle.length;
 
-    for (let row: number = triangleHeight - 2; row >= 0; row--) {
-        const triangleLength = triangle[row].length;
+  for (let row: number = ROWS - 2; row >= 0; row--) {
+    const COLUMNS = triangle[row].length;
 
-        for (let column: number = 0; column < triangleLength; column++) {
-            const firstOption: number = triangle[row + 1][column];
-            const secondOption: number = triangle[row + 1][column + 1];
+    for (let column: number = 0; column < COLUMNS; column++) {
+      const firstOption: number = triangle[row + 1][column];
+      const secondOption: number = triangle[row + 1][column + 1];
 
-            triangle[row][column] += minValue(firstOption, secondOption);
-        }
+      triangle[row][column] += minValue(firstOption, secondOption);
     }
+  }
 
-    return triangle[0][0];
+  return triangle[0][0];
 }
