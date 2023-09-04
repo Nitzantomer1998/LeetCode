825acacde247d563cc06785908d54086a0daf4ba
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-04 08:36:50 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/136. Single Number.java
+++ Java/Algorithm/Algorithm I/136. Single Number.java
@@ -1,5 +1,11 @@
 class Solution {
 
+  /**
+   * Finds the single number in an array where every other element appears twice.
+   *
+   * @param nums An array of integers where all elements appear twice except one.
+   * @return The unique number that appears only once.
+   */
   public int singleNumber(int[] nums) {
     int NUMS_LENGTH = nums.length;
 
