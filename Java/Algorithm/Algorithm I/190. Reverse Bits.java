56e63633e150002b2350d0547d644dba5c7e0d01
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-06 08:03:50 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/190. Reverse Bits.java
+++ Java/Algorithm/Algorithm I/190. Reverse Bits.java
@@ -1,5 +1,11 @@
 public class Solution {
 
+  /**
+   * Reverses the bits of a 32-bit integer.
+   *
+   * @param n The 32-bit integer to reverse its bits.
+   * @return The integer with reversed bits.
+   */
   public int reverseBits(int n) {
     int reversedN = 0;
 
