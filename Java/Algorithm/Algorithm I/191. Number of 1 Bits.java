83cf5241103170c6898251e53af14db09f3d2c37
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-06 08:02:56 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/191. Number of 1 Bits.java
+++ Java/Algorithm/Algorithm I/191. Number of 1 Bits.java
@@ -1,5 +1,11 @@
 public class Solution {
 
+  /**
+   * Counts the number of '1' bits in a 32-bit integer.
+   *
+   * @param n The 32-bit integer to count the '1' bits.
+   * @return The number of '1' bits in the integer.
+   */
   public int hammingWeight(int n) {
     int oneBitCounter = 0;
 
