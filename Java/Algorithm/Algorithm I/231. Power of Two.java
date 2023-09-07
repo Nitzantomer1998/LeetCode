92f0a3deb4dbf7a3c4fd8018f480246d2f25668b
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-07 08:53:24 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/231. Power of Two.java
+++ Java/Algorithm/Algorithm I/231. Power of Two.java
@@ -1,5 +1,11 @@
 class Solution {
 
+  /**
+   * Checks if a given integer is a power of two.
+   *
+   * @param n The integer to check.
+   * @return True if the integer is a power of two, otherwise false.
+   */
   public boolean isPowerOfTwo(int n) {
     boolean isPositive = n > 0;
     boolean isTwoPower = (n & (n - 1)) == 0;
