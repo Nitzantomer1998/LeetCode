5b5ed85081f9349a2edb079b318e082e5096150f
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-07 08:52:32 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/206. Reverse Linked List.java
+++ Java/Algorithm/Algorithm I/206. Reverse Linked List.java
@@ -1,5 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
 
+  /**
+   * Reverses a singly-linked list in place.
+   *
+   * @param head The head node of the linked list to be reversed.
+   * @return The head node of the reversed linked list.
+   */
   public ListNode reverseList(ListNode head) {
     ListNode currentNode = head;
     ListNode previousNode = null;
