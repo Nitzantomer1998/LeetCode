851469b27ccdfbfeae4a62a86b7857a6b2245f0c
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-07 08:51:12 +0300

    Adding Problem 21 Solution

---
+++ Java/Algorithm/Algorithm I/21. Merge Two Sorted Lists.java
@@ -0,0 +1,25 @@
+class Solution {
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    ListNode head = new ListNode();
+    ListNode mergedList = head;
+
+    while (list1 != null && list2 != null) {
+      if (list1.val < list2.val) {
+        mergedList.next = list1;
+        list1 = list1.next;
+      } 
+      
+      else {
+        mergedList.next = list2;
+        list2 = list2.next;
+      }
+
+      mergedList = mergedList.next;
+    }
+
+    if (list1 != null) mergedList.next = list1;
+    if (list2 != null) mergedList.next = list2;
+
+    return head.next;
+  }
+}
