fd6527a0e428071453e76e693f70e539766246e7
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-08 11:56:36 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/283. Move Zeroes.java
+++ Java/Algorithm/Algorithm I/283. Move Zeroes.java
@@ -1,5 +1,10 @@
 class Solution {
 
+  /**
+   * Moves all zeroes to the end of an integer array while maintaining the relative order of non-zero elements.
+   *
+   * @param nums An array of integers containing zeroes and non-zeroes.
+   */
   public void moveZeroes(int[] nums) {
     int NUMS_LENGTH = nums.length;
     int nonZeroIndex = 0;
