04243764bce06de03d89ccaade5aa95390d62354
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-08 11:55:18 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/278. First Bad Version.java
+++ Java/Algorithm/Algorithm I/278. First Bad Version.java
@@ -1,5 +1,14 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+   boolean isBadVersion(int version); */
+
 public class Solution extends VersionControl {
 
+  /**
+   * Finds the first bad version in a range of versions using binary search.
+   *
+   * @param n The highest version number.
+   * @return The first bad version found.
+   */
   public int firstBadVersion(int n) {
     int leftPointer = 1;
     int rightPointer = n;
