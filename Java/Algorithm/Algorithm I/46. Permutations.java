d07c31c4ac194fa38acc78f2269f40f1d9aedfd0
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-10 08:28:45 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/46. Permutations.java
+++ Java/Algorithm/Algorithm I/46. Permutations.java
@@ -1,5 +1,12 @@
 class Solution {
 
+  /**
+   * Backtracking helper method to generate all permutations of an integer array.
+   *
+   * @param permutatedList The list to store generated permutations.
+   * @param tempList       The temporary list used for generating permutations.
+   * @param nums           The input array of integers.
+   */
   private void backtrack(List<List<Integer>> permutatedList, List<Integer> tempList, int[] nums) {
     int NUMS_LENGTH = nums.length;
 
@@ -18,6 +25,12 @@ class Solution {
     }
   }
 
+  /**
+   * Generates all permutations of the given integer array.
+   *
+   * @param nums The input array of integers.
+   * @return A list of lists containing all possible permutations of the input array.
+   */
   public List<List<Integer>> permute(int[] nums) {
     List<List<Integer>> permutatedList = new ArrayList<>();
 
