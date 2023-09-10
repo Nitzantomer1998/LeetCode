0d043e33487c4c86f104d6c8a520a5057926abfa
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-10 08:27:28 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/35. Search Insert Position.java
+++ Java/Algorithm/Algorithm I/35. Search Insert Position.java
@@ -1,5 +1,13 @@
 class Solution {
 
+  /**
+   * Searches for the target element in a sorted array and returns its index if found.
+   * If the target is not found, returns the index where it would be inserted while maintaining the sorted order.
+   *
+   * @param nums   The sorted array of integers to search in.
+   * @param target The target element to search for.
+   * @return The index of the target element if found, or the index to insert it if not found.
+   */
   public int searchInsert(int[] nums, int target) {
     int NUMS_LENGTH = nums.length;
 
