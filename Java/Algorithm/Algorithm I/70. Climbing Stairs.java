b948cd9bbf1d2b2700ee65081e49ce1bfcc4559b
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-12 16:46:54 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/70. Climbing Stairs.java
+++ Java/Algorithm/Algorithm I/70. Climbing Stairs.java
@@ -1,5 +1,11 @@
 class Solution {
 
+  /**
+   * Computes the number of distinct ways to climb to the top of n steps.
+   *
+   * @param n The total number of steps to climb.
+   * @return The number of distinct ways to reach the top.
+   */
   public int climbStairs(int n) {
     int previousStep = 0;
     int currentStep = 1;
