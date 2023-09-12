c8a4c761dd6020457f4a7d96a3823d6bc92885c4
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-12 16:47:45 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/617. Merge Two Binary Trees.java
+++ Java/Algorithm/Algorithm I/617. Merge Two Binary Trees.java
@@ -1,5 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 class Solution {
 
+  /**
+   * Merges two binary trees by summing the corresponding nodes.
+   *
+   * @param root1 The root of the first binary tree.
+   * @param root2 The root of the second binary tree.
+   * @return The merged binary tree.
+   */
   public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
     if (root1 == null && root2 == null) return null;
 
