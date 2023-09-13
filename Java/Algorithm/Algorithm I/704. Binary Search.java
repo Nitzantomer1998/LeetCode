69bab77da76a337335adaaa183beff79ac2c095d
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-13 09:42:48 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/704. Binary Search.java
+++ Java/Algorithm/Algorithm I/704. Binary Search.java
@@ -1,5 +1,12 @@
 class Solution {
 
+  /**
+   * Searches for the target element in a sorted array using binary search.
+   *
+   * @param nums   The sorted array to search in.
+   * @param target The target element to find.
+   * @return The index of the target element if found, otherwise -1.
+   */
   public int search(int[] nums, int target) {
     int NUMS_LENGTH = nums.length;
 
