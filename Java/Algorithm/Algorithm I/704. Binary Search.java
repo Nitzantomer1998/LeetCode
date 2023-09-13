b768420bb1bcf20ae4e8225826025d44618f9e67
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-13 09:42:35 +0300

    Adding Problem 704 Solution

---
+++ Java/Algorithm/Algorithm I/704. Binary Search.java
@@ -0,0 +1,25 @@
+class Solution {
+
+  public int search(int[] nums, int target) {
+    int NUMS_LENGTH = nums.length;
+
+    int leftPointer = 0;
+    int rightPointer = NUMS_LENGTH - 1;
+
+    while (leftPointer <= rightPointer) {
+      int middlePointer = leftPointer + (rightPointer - leftPointer) / 2;
+      int middleValue = nums[middlePointer];
+
+      if (middleValue == target) 
+        return middlePointer; 
+        
+      else if (middleValue > target) 
+        rightPointer = middlePointer - 1; 
+        
+      else 
+        leftPointer = middlePointer + 1;
+    }
+
+    return -1;
+  }
+}
