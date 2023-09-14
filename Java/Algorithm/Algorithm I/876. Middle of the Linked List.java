331f86b595efa66cf401133fbb7ac8385324f060
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-14 09:36:22 +0300

    Adding Notes

--- Java/Algorithm/Algorithm I/876. Middle of the Linked List.java
+++ Java/Algorithm/Algorithm I/876. Middle of the Linked List.java
@@ -1,5 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
 
+  /**
+   * Finds the middle node of a singly-linked list.
+   *
+   * @param head The head of the linked list.
+   * @return The middle node of the linked list.
+   */
   public ListNode middleNode(ListNode head) {
     ListNode fastList = head;
     ListNode slowList = head;
