97b727de5b51f9fe4e29bdc9857844da35f999ce
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-09-18 08:54:37 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm II/1091. Shortest Path in Binary Matrix.ts
+++ TypeScript/Algorithm/Algorithm II/1091. Shortest Path in Binary Matrix.ts
@@ -1,3 +1,12 @@
+/**
+ * Checks if a given cell is valid within the grid.
+ *
+ * @param row - The row index of the cell.
+ * @param column - The column index of the cell.
+ * @param ROWS - The total number of rows in the grid.
+ * @param COLUMNS - The total number of columns in the grid.
+ * @returns `true` if the cell is valid, otherwise `false`.
+ */
 function isValidCell(row: number, column: number, ROWS: number, COLUMNS: number): boolean {
   const isValidRow: boolean = row >= 0 && row < ROWS;
   const isValidColumn: boolean = column >= 0 && column < COLUMNS;
@@ -5,6 +14,12 @@ function isValidCell(row: number, column: number, ROWS: number, COLUMNS: number)
   return isValidRow && isValidColumn;
 }
 
+/**
+ * Finds the shortest path in a binary matrix from the top-left corner to the bottom-right corner.
+ *
+ * @param grid - The binary matrix representing the grid.
+ * @returns The length of the shortest path from (0, 0) to (ROWS-1, COLUMNS-1), or -1 if no path exists.
+ */
 function shortestPathBinaryMatrix(grid: number[][]): number {
   const ROWS: number = grid.length;
   const COLUMNS: number = grid[0].length;
