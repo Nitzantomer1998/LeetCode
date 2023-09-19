6565479c8b031e925f70bf785d6617c96876de28
Nitzan <nitzantomer1998@gmail.com>
2023-09-19 08:04:41 +0300

    Adding Notes

--- C/Algorithm/Algorithm II/11. Container With Most Water.c
+++ C/Algorithm/Algorithm II/11. Container With Most Water.c
@@ -1,7 +1,25 @@
+/* Definition for getting the minimum value */
 #define MIN(X, Y) (X < Y ? X : Y)
 
+/* Definition for getting the maximum value */
 #define MAX(X, Y) (X > Y ? X : Y)
 
+/*
+ * Calculates the maximum area between vertical bars.
+ *
+ * The 'maxArea' function takes an array 'height' representing the heights of vertical bars
+ * and its size 'heightSize' as input. It calculates the maximum area between two vertical bars
+ * by using a two-pointer approach. The function iterates through the array while adjusting the
+ * pointers to maximize the area between the bars. The result is the maximum area that can be
+ * enclosed by two bars.
+ *
+ * Parameters:
+ * - height: An array representing the heights of vertical bars.
+ * - heightSize: The number of elements in the 'height' array.
+ *
+ * Returns:
+ * The maximum area between two vertical bars.
+ */
 int maxArea(int *height, int heightSize)
 {
     int HEIGHT_LENGTH = heightSize;
