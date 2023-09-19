ea702c0142fa13b0229aa213230d61b74398a2f4
Nitzan <nitzantomer1998@gmail.com>
2023-09-19 08:01:02 +0300

    Adding Notes

--- Python/Algorithm/Algorithm II/11. Container With Most Water.py
+++ Python/Algorithm/Algorithm II/11. Container With Most Water.py
@@ -3,6 +3,15 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        """
+        Find the maximum area between two bars in a container.
+
+        Args:
+            heightList (List[int]): A list of integers representing the heights of the bars.
+
+        Returns:
+            int: The maximum area between two bars in the container.
+        """
         HEIGHT_LENGTH = len(height)
 
         maxContainer = 0
