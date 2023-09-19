2b8a73bd967eb28ead3748450e9566a69a190856
Nitzan <nitzantomer1998@gmail.com>
2023-09-19 08:00:48 +0300

    Adding Problem 11 Solution

--- Python/Algorithm/Algorithm II/11. Container With Most Water.py
+++ Python/Algorithm/Algorithm II/11. Container With Most Water.py
@@ -1,34 +1,26 @@
-def max_area(heights: list[int]) -> int:
-    """
-    Finding the container with the maximum area, and return it
-    Note : A container is made by two different heights from heights
+from typing import List
 
-    :param heights: List of integers
-    :return: The container with the maximum area
 
-    Time Complexity: o(n)
-    Space Complexity: o(1)
-    """
-    # Integer storing the highest possible area value
-    max_area_value = 0
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        HEIGHT_LENGTH = len(height)
 
-    # Left & Right pointer to the start & end indices in heights
-    left, right = 0, len(heights) - 1
+        maxContainer = 0
 
-    # Loop for finding the maximum area value, starting from the edges and slowly shrink itself
-    while left < right:
+        leftPointer = 0
+        rightPointer = HEIGHT_LENGTH - 1
 
-        # Calculating the current area value, and then updating the max_area value if needed
-        current_area = (right - left) * min(heights[left], heights[right])
-        max_area_value = max(max_area_value, current_area)
+        while leftPointer < rightPointer:
+            leftBar = height[leftPointer]
+            rightBar = height[rightPointer]
+            barDistance = rightPointer - leftPointer
 
-        # if heights[left] smaller than height[right], than update left pointer to move one step forward
-        if heights[left] < heights[right]:
-            left += 1
+            currentContainer = min(leftBar, rightBar) * barDistance
+            maxContainer = max(maxContainer, currentContainer)
 
-        # if heights[right] smaller or equal to height[left], than update right pointer to move one step backward
-        else:
-            right -= 1
+            if leftBar < rightBar:
+                leftPointer += 1
+            else:
+                rightPointer -= 1
 
-    # Returning the area of the maximum container
-    return max_area_value
+        return maxContainer
