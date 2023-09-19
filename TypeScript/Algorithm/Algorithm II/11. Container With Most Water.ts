705849104e310596a6347a78711a823c4b4c49fe
Nitzan <nitzantomer1998@gmail.com>
2023-09-19 07:59:15 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm II/11. Container With Most Water.ts
+++ TypeScript/Algorithm/Algorithm II/11. Container With Most Water.ts
@@ -1,3 +1,10 @@
+/**
+ * Given an array of heights, this function calculates the maximum area of a container
+ * that can be formed by selecting two bars and the area between them.
+ *
+ * @param height - An array of heights representing the bars.
+ * @returns The maximum area of a container that can be formed.
+ */
 function maxArea(height: number[]): number {
   const HEIGHT_LENGTH: number = height.length;
 
