732b4718859466fc6a9848be9011811ae7b984a0
Nitzan <nitzantomer1998@gmail.com>
2023-09-21 08:23:45 +0300

    Adding Notes

--- Python/Algorithm/Algorithm II/130. Surrounded Regions.py
+++ Python/Algorithm/Algorithm II/130. Surrounded Regions.py
@@ -1,53 +1,73 @@
-def solve(board: list[list[str]]) -> None:
-    """
-    Modifying the matrix such that every region that surround by X will update to X as well
-    Note : Region is a group of 4-directionally with the value O that surrounded by 'X'
+class Solution:
 
-    :param board: Matrix contain X and O
-    :return: Nothing, None, Everything happen in place
-
-    Time Complexity: o(n * m)
-    Space Complexity: o(n * m)
-    """
-    # Constants for the cells indices boundaries, for more readable code
-    MAX_ROW = len(board)
-    MAX_COLUMN = len(board[0])
-
-    # Assisting function to make the DFS calls
-    def capture(row: int, column: int) -> None:
+    def isValidCell(self, board: list[list[int]], row: int, column: int) -> bool:
         """
-        Recursive function for checking if the current indices is part or a region using DFS Algorithm
+        Check if a cell is a valid cell within the board.
 
-        :param row: Integer represent the current cell row
-        :param column: Integer represent the current cell column
-        :return: None, Everything happen in place
+        Args:
+            board (List[List[int]]): The board represented as a list of lists of integers.
+            row (int): The row index of the cell.
+            column (int): The column index of the cell.
+
+        Returns:
+            bool: True if the cell is valid, False otherwise.
         """
-        # if the sent indices are invalid or the cell is not part of the region, return
-        if not (0 <= row < MAX_ROW and 0 <= column < MAX_COLUMN) or board[row][column] != 'O':
+        ROWS = len(board)
+        COLUMNS = len(board[0])
+
+        isValidRow = 0 <= row < ROWS
+        isValidColumn = 0 <= column < COLUMNS
+
+        return isValidRow and isValidColumn
+
+    def dfs(self, board: list[list[int]], row: int, column: int) -> None:
+        """
+        Perform Depth-First Search (DFS) to mark connected 'O' cells as 'U'.
+
+        Args:
+            board (List[List[int]]): The board represented as a list of lists of integers.
+            row (int): The current row index.
+            column (int): The current column index.
+
+        Returns:
+            None
+        """
+        if self.isValidCell(board, row, column) == False:
             return
+        
+        if board[row][column] != 'O':
+            return
+        
+        board[row][column] = 'U'
 
-        # the current cell is part of the region, update it to special symbol T
-        board[row][column] = 'T'
+        self.dfs(board, row - 1, column)
+        self.dfs(board, row + 1, column)
+        self.dfs(board, row, column - 1)
+        self.dfs(board, row, column + 1)
 
-        # 4 Callbacks to the other possible regions in the horizontal vertical axis
-        capture(row - 1, column)
-        capture(row + 1, column)
-        capture(row, column - 1)
-        capture(row, column + 1)
+    def solve(self, board: list[list[str]]) -> None:
+        """
+        Solve the 'Surrounded Regions' problem by capturing surrounded regions.
 
-    # 1. (DFS) Capture un-surrounded regions and update value (O -> T)
-    # Traverse the border, if there's O check for region and update their values for T (symbol for un-surrounded)
-    for row in range(MAX_ROW):
-        for column in range(MAX_COLUMN):
-            if board[row][column] == 'O' and (row in [0, MAX_ROW - 1] or column in [0, MAX_COLUMN - 1]):
-                capture(row, column)
+        Args:
+            board (List[List[str]]): The board represented as a list of lists of characters.
 
-    # 2. Capture surrounded regions and update value (O -> X), also update un-surrounded regions (T -> O)
-    for row in range(MAX_ROW):
-        for column in range(MAX_COLUMN):
+        Returns:
+            None
+        """
+        ROWS = len(board)
+        COLUMNS = len(board[0])
 
-            if board[row][column] == 'O':
-                board[row][column] = 'X'
+        for row in range(ROWS):
+            for column in range(COLUMNS):
+                if row in [0, ROWS - 1] or column in [0, COLUMNS - 1]:
+                    if board[row][column] == 'O':
+                        self.dfs(board, row, column)
 
-            elif board[row][column] == 'T':
-                board[row][column] = 'O'
+        for row in range(ROWS):
+            for column in range(COLUMNS):
+                if board[row][column] == 'O':
+                    board[row][column] = 'X'
+
+                elif board[row][column] == 'U':
+                    board[row][column] = 'O'
