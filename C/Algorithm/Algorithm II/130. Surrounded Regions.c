ae787e8ce4d74a1bde346c6d5bb4b948f414ccca
Nitzan <nitzantomer1998@gmail.com>
2023-09-21 08:16:11 +0300

    Adding Notes

--- C/Algorithm/Algorithm II/130. Surrounded Regions.c
+++ C/Algorithm/Algorithm II/130. Surrounded Regions.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/*
+ * Checks if a cell is a valid cell in the grid.
+ *
+ * The 'isValidCell' function takes the row and column indices of a cell, as well as the total
+ * number of rows ('ROWS') and columns ('COLUMNS') in the grid. It returns 'true' if the cell is
+ * within the grid boundaries, and 'false' otherwise.
+ *
+ * Parameters:
+ * - row: The row index of the cell.
+ * - column: The column index of the cell.
+ * - ROWS: The total number of rows in the grid.
+ * - COLUMNS: The total number of columns in the grid.
+ *
+ * Returns:
+ * 'true' if the cell is valid, 'false' otherwise.
+ */
 bool isValidCell(int row, int column, int ROWS, int COLUMNS)
 {
     bool isValidRow = row >= 0 && row < ROWS;
@@ -10,6 +26,20 @@ bool isValidCell(int row, int column, int ROWS, int COLUMNS)
     return isValidRow && isValidColumn;
 }
 
+/*
+ * Depth-First Search (DFS) traversal to mark 'O' regions.
+ *
+ * The 'dfs' function performs a depth-first search starting from a cell at the specified row and
+ * column indices in the 'board'. It marks connected 'O' cells as 'U' to indicate they are part of
+ * an 'O' region. The function continues the traversal recursively in all valid directions.
+ *
+ * Parameters:
+ * - board: A pointer to a 2D character array representing the board.
+ * - row: The current row index.
+ * - column: The current column index.
+ * - ROWS: The total number of rows in the board.
+ * - COLUMNS: The total number of columns in the board.
+ */
 void dfs(char ***board, int row, int column, int ROWS, int COLUMNS)
 {
     if (isValidCell(row, column, ROWS, COLUMNS) == false)
@@ -26,6 +56,19 @@ void dfs(char ***board, int row, int column, int ROWS, int COLUMNS)
     dfs(board, row, column + 1, ROWS, COLUMNS);
 }
 
+/*
+ * Solves the surrounded regions on the board.
+ *
+ * The 'solve' function takes a 2D character array 'board', its size 'boardSize', and an array
+ * 'boardColSize' representing the number of columns in each row. It identifies and marks the 'O'
+ * regions on the board that are surrounded by 'X'. The 'dfs' function is used to perform depth-first
+ * search to mark the 'O' regions.
+ *
+ * Parameters:
+ * - board: A 2D character array representing the board.
+ * - boardSize: The number of rows in the board.
+ * - boardColSize: An array representing the number of columns in each row of the board.
+ */
 void solve(char **board, int boardSize, int *boardColSize)
 {
     int ROWS = boardSize;
