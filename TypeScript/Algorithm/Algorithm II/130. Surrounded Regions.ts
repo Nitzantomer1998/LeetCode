f1411a93f743bc8345074e97e203a0970dae61e7
Nitzan <nitzantomer1998@gmail.com>
2023-09-21 08:25:08 +0300

    Adding Notes

--- TypeScript/Algorithm/Algorithm II/130. Surrounded Regions.ts
+++ TypeScript/Algorithm/Algorithm II/130. Surrounded Regions.ts
@@ -1,3 +1,10 @@
+/**
+ * Checks if a given cell is a valid cell within the board.
+ * @param board - The game board represented as a 2D array of strings.
+ * @param row - The row index of the cell.
+ * @param column - The column index of the cell.
+ * @returns True if the cell is valid, otherwise false.
+ */
 function isValidCell(board: string[][], row: number, column: number): boolean {
   const ROWS: number = board.length;
   const COLUMNS: number = board[0].length;
@@ -8,6 +15,12 @@ function isValidCell(board: string[][], row: number, column: number): boolean {
   return isValidRow && isValidColumn;
 }
 
+/**
+ * Depth-first search (DFS) algorithm to recursively traverse and mark 'O' connected cells as 'U'.
+ * @param board - The game board represented as a 2D array of strings.
+ * @param row - The current row index.
+ * @param column - The current column index.
+ */
 function dfs(board: string[][], row: number, column: number): void {
   if (isValidCell(board, row, column) == false) 
     return;
@@ -23,6 +36,10 @@ function dfs(board: string[][], row: number, column: number): void {
   dfs(board, row, column + 1);
 }
 
+/**
+ * Solves the 'X' surrounded by 'O' problem on the game board.
+ * @param board - The game board represented as a 2D array of strings.
+ */
 function solve(board: string[][]): void {
   const ROWS: number = board.length;
   const COLUMNS: number = board[0].length;
