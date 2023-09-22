a49e6c1e46884850cee047c5e71bf1c040493a11
Nitzan <nitzantomer1998@gmail.com>
2023-09-22 16:53:31 +0300

    Adding Notes

--- Python/Algorithm/Algorithm II/117. Populating Next Right Pointers in Each Node II.py
+++ Python/Algorithm/Algorithm II/117. Populating Next Right Pointers in Each Node II.py
@@ -1,56 +1,41 @@
 import collections
 
 
-# Class provided by LeetCode for the following problem
-class Node:
-    def __init__(self, value: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.value = value
-        self.left = left
-        self.right = right
-        self.next = next
+# Definition for a Node.
+# class Node:
+#     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#         self.next = next
 
 
-def connect(root: 'Node') -> 'Node':
-    """
-    Populate each next pointer to point to its next right node if exists, else null, and return the binary tree
-    Note : using bfs algorithm
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        """
+        Connect nodes at the same level in a binary tree with a next pointer.
 
-    :param root: Root node of a binary tree
-    :return: The modified binary tree
+        Args:
+            root (Node): The root node of the binary tree.
 
-    Time Complexity: o(n)
-    Space Complexity: o(log(n))
-    """
-    # dequeue for bfs algorithm, Initialize with the root of the tree
-    queue = collections.deque([root])
+        Returns:
+            Node: The root node of the modified binary tree with next pointers.
+        """
+        queue = collections.deque([root])
 
-    # while loop for updating each level of node in the binary tree, queue[0] in order to avoid empty tree
-    while queue and queue[0]:
+        while queue and queue[0]:
+            currentLevel = len(queue)
 
-        # Node storing the left node of the current iteration
-        # Note : First node in each level is always None
-        previous_node = None
+            for level in range(currentLevel):
+                currentNode = queue.popleft()
 
-        # Loop to update every node in this current binary tree level
-        for _ in range(len(queue)):
+                if level < currentLevel - 1:
+                    currentNode.next = queue[0]
 
-            # Getting the first node from the queue
-            current_node = queue.popleft()
+                if currentNode.left:
+                    queue.append(currentNode.left)
 
-            # if the previous node isn't None, means we need to update the previous next pointer to be the current node
-            if previous_node:
-                previous_node.next = current_node
+                if currentNode.right:
+                    queue.append(currentNode.right)
 
-            # if the current node has a left child, add it to the queue
-            if current_node.left:
-                queue.append(current_node.left)
-
-            # if the current node has a right child, add it to the queue
-            if current_node.right:
-                queue.append(current_node.right)
-
-            # Update the previous node to be the current for the next iteration
-            previous_node = current_node
-
-    # Return the modified binary tree
-    return root
+        return root
