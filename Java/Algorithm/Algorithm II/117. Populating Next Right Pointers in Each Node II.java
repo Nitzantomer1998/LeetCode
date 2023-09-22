2e328da0ac55afb87897e9a340bd19d52ff76193
Nitzan <nitzantomer1998@gmail.com>
2023-09-22 16:54:26 +0300

    Adding Notes

--- Java/Algorithm/Algorithm II/117. Populating Next Right Pointers in Each Node II.java
+++ Java/Algorithm/Algorithm II/117. Populating Next Right Pointers in Each Node II.java
@@ -1,6 +1,35 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+/**
+ * Connects each node in a binary tree to its right neighbor.
+ *
+ * @param root The root node of the binary tree.
+ * @return The root node of the modified binary tree with connected neighbors.
+ */
 class Solution {
 
   public Node connect(Node root) {
