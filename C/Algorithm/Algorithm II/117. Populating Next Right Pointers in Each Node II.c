d8c443adffca7bd0f4977b3338bbfa125bb41322
Nitzan <nitzantomer1998@gmail.com>
2023-09-22 16:55:19 +0300

    Adding Notes

--- C/Algorithm/Algorithm II/117. Populating Next Right Pointers in Each Node II.c
+++ C/Algorithm/Algorithm II/117. Populating Next Right Pointers in Each Node II.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+/** 
+ * Definition of a Node
+ * struct Node
+ * {
+ *  int val;
+ *  struct Node *right;
+ *  struct Node *left;
+ *  struct Node *next;
+ * };
+*/
 
+/*
+ * Traverses a binary tree and connects nodes at the same level.
+ *
+ * The 'traverse' function takes the root node of a binary tree 'root', an array 'previousNode'
+ * to track the previous node at each level, the 'level' of the current node, and the 'treeHeight'
+ * as input. It recursively traverses the tree and connects nodes at the same level by updating the
+ * 'next' pointers. It utilizes a level-order traversal approach.
+ *
+ * Parameters:
+ * - root: The root node of the binary tree.
+ * - previousNode: An array to track the previous node at each level.
+ * - level: The current level in the tree.
+ * - treeHeight: The height of the binary tree.
+ */
 void traverse(struct Node *root, struct Node **previousNode, int level, int treeHeight)
 {
   if (root == NULL)
@@ -18,6 +42,19 @@ void traverse(struct Node *root, struct Node **previousNode, int level, int tree
   traverse(root->right, previousNode, level + 1, treeHeight);
 }
 
+/*
+ * Calculates the height of a binary tree.
+ *
+ * The 'height' function takes the root node of a binary tree 'root' as input and calculates the
+ * height of the binary tree by recursively finding the maximum height of the left and right
+ * subtrees and adding 1 to it.
+ *
+ * Parameters:
+ * - root: The root node of the binary tree.
+ *
+ * Returns:
+ * The height of the binary tree.
+ */
 int height(struct Node *root)
 {
   if (root == NULL)
@@ -29,6 +66,20 @@ int height(struct Node *root)
   return (leftHeight > rightHeight) ? (leftHeight + 1) : (rightHeight + 1);
 }
 
+/*
+ * Connects nodes at the same level in a binary tree.
+ *
+ * The 'connect' function takes the root node of a binary tree 'root' as input and connects nodes
+ * at the same level by calling the 'traverse' function with appropriate arguments. It allocates
+ * memory for an array 'previousNode' to track the previous node at each level and then frees
+ * the memory before returning the root node.
+ *
+ * Parameters:
+ * - root: The root node of the binary tree.
+ *
+ * Returns:
+ * The root node of the binary tree with 'next' pointers connected.
+ */
 struct Node *connect(struct Node *root)
 {
   int treeHeight = height(root);
