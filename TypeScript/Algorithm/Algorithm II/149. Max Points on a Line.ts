dd7b8eedca159c23cb6cb8c110852950c26a8ae4
Nitzan <nitzantomer1998@gmail.com>
2023-09-26 13:17:31 +0300

    Adding Problem 149 Solution

---
+++ TypeScript/Algorithm/Algorithm II/149. Max Points on a Line.ts
@@ -0,0 +1,32 @@
+function maxPoints(points: number[][]): number {
+  const POINTS_LENGTH: number = points.length;
+
+  let maxPoints: number = 0;
+
+  for (let i: number = 0; i < POINTS_LENGTH; i++) {
+    const slopeHash: { [key: string]: number } = {};
+
+    let localMax = 0;
+
+    for (let j: number = i + 1; j < POINTS_LENGTH; j++) {
+      const firstPoint: number[] = points[i];
+      const secondPoint: number[] = points[j];
+
+      const X1: number = firstPoint[0];
+      const Y1: number = firstPoint[1];
+      const X2: number = secondPoint[0];
+      const Y2: number = secondPoint[1];
+
+      const currentSlope: string =
+        X1 === X2 ? "vertical" : ((Y2 - Y1) / (X2 - X1)).toString();
+
+      slopeHash[currentSlope] = (slopeHash[currentSlope] || 0) + 1;
+
+      localMax = Math.max(localMax, slopeHash[currentSlope]);
+    }
+
+    maxPoints = Math.max(maxPoints, localMax + 1);
+  }
+
+  return maxPoints;
+}
