784538e589622ef0d38eda75b9a5a17fa44b318b
Nitzan <nitzantomer1998@gmail.com>
2023-09-28 13:59:22 +0300

    Adding Notes

--- Python/Algorithm/Algorithm II/153. Find Minimum in Rotated Sorted Array.py
+++ Python/Algorithm/Algorithm II/153. Find Minimum in Rotated Sorted Array.py
@@ -1,36 +1,31 @@
-def find_min(numbers: list[int]) -> int:
-    """
-    Finding the value of the minimum integer in numbers list, and return it
-    Note : numbers is sorted list but its may have been shift rotated right
+from typing import List
 
-    :param numbers: Sorted integer list, that might have been shift rotated right
-    :return: The value of the minimum integer in numbers list
 
-    Time Complexity: o(log(n))
-    Space Complexity: o(1)
-    """
-    # Binary Search Algorithm
-    # Left & Right pointer to traverse the sorted list
-    left, right = 0, len(numbers) - 1
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        """
+        Find the minimum element in a rotated sorted array.
 
-    # Loop to traverse the list
-    while left <= right:
+        Args:
+            nums (List[int]): A list of integers representing a rotated sorted array.
 
-        # Integer storing the middle index
-        middle = (left + right) // 2
+        Returns:
+            int: The minimum element in the array.
+        """
+        NUMS_LENGTH = len(nums)
 
-        # if numbers[left, middle] is an ascending order
-        if numbers[left] <= numbers[middle]:
+        left = 0
+        right = NUMS_LENGTH - 1
 
-            # if numbers[left] is truly the minimum value return it
-            if numbers[left] <= numbers[right]:
-                return numbers[left]
+        while left <= right:
+            middle = left + (right - left) // 2
 
-            # if numbers[left] wasn't the minimum value, then update left to be middle + 1
+            if nums[left] <= nums[middle]:
+                if nums[left] <= nums[right]:
+                    return nums[left]
+
+                else:
+                    left = middle + 1
+
             else:
-                left = middle + 1
-
-        # if numbers[left, middle] wasn't in ascending order means the minimum value is in numbers[left, middle]
-        # Update right to be middle
-        else:
-            right = middle
+                right = middle
