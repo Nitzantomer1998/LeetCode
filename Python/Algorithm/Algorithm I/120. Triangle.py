dbf6f4c0f925711513f835d2472ab57d4ff7b0c0
Nitzan <nitzantomer1998@gmail.com>
2023-09-29 16:36:09 +0300

    Adding Notes

--- Python/Algorithm/Algorithm I/120. Triangle.py
+++ Python/Algorithm/Algorithm I/120. Triangle.py
@@ -1,25 +1,26 @@
-def minimum_total(triangle: list[list[int]]) -> int:
-    """
-    Finding the minimum path sum from top to bottom of a triangle array, and return it
-    Note: From each [row][column] you may proceed by going [row + 1][column] or [row + 1][column + 1]
+from typing import List
 
-    :param triangle: List of List of integers
-    :return: The minimum path sum from top to bottom of a triangle array
 
-    Time Complexity: o(n^2)
-    Space Complexity: o(1)
-    """
-    # Loop to start from the bottom of the triangle and moving up
-    for row in reversed(range(len(triangle))):
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        """
+        Find the minimum path sum from top to bottom in a triangle.
 
-        # Loop to traverse the indices of the current row
-        for column in range(len(triangle[row]) - 1):
-            # Variables to store the calculation of the two option to get to triangle[row - 1][column]
-            first_option = triangle[row - 1][column] + triangle[row][column]
-            second_option = triangle[row - 1][column] + triangle[row][column + 1]
+        Args:
+            triangle (List[List[int]]): A list of lists representing the triangle.
 
-            # Updating the triangle[row - 1][column] value to be the minimum sum to get to him
-            triangle[row - 1][column] = min(first_option, second_option)
+        Returns:
+            int: The minimum path sum.
+        """
+        ROWS = len(triangle)
 
-    # Returning the minimum sum to get from top to bottom
-    return triangle[0][0]
+        for row in range(ROWS - 2, -1, -1):
+            COLUMNS = len(triangle[row])
+
+            for column in range(COLUMNS):
+                firstOption = triangle[row + 1][column]
+                secondOption = triangle[row + 1][column + 1]
+
+                triangle[row][column] += min(firstOption, secondOption)
+
+        return triangle[0][0]
