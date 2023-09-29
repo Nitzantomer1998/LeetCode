58c1ad1ee45a89e3465a574b912a220d9a509cb5
Nitzan <nitzantomer1998@gmail.com>
2023-09-29 16:36:22 +0300

    Adding Notes

--- Python/Algorithm/Algorithm I/167. Two Sum II - Input Array Is Sorted.py
+++ Python/Algorithm/Algorithm I/167. Two Sum II - Input Array Is Sorted.py
@@ -1,33 +1,31 @@
-def two_sum(numbers: list[int], target: int) -> list[int]:
-    """
-    Finding two indices in numbers which their summation is equal to target, and return their indices
-    Note: numbers is sorted by non-decreasing order
+from typing import List
 
-    :param numbers: List of integers
-    :param target: Integer represent target to achieve by summation of two integers in numbers
-    :return: Two indices in numbers which their summation is equal to target
 
-    Time Complexity: o(n)
-    Space Complexity: o(1)
-    """
-    # Left & Right pointers for the start and the end of numbers
-    left, right = 0, len(numbers) - 1
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        Find two numbers in a sorted array that add up to a specific target.
 
-    # Loop to traverse the pointers left and right, in order to achieve the desire target
-    while left < right:
+        Args:
+            numbers (List[int]): A list of integers.
+            target (int): The target sum.
 
-        current_sum = numbers[left] + numbers[right]
+        Returns:
+            List[int]: A list containing the indices of the two numbers that add up to the target.
+        """
+        NUMBERS_LENGTH = len(numbers)
 
-        # if the current sum is larger than target, than we need to lower the sum, therefor right moving to the left
-        if current_sum > target:
-            right -= 1
+        leftPointer = 0
+        rightPointer = NUMBERS_LENGTH - 1
 
-        # if the current sum is lower than target, than we need to enlarge the sum, therefor left moving to the right
-        elif current_sum < target:
-            left += 1
+        while leftPointer < rightPointer:
+            currentSum = numbers[leftPointer] + numbers[rightPointer]
 
-        # We got the desirable target, return the solution
-        else:
+            if currentSum == target:
+                return [leftPointer + 1, rightPointer + 1]
 
-            # Returning the solution indices, Note: LeetCode first index start from 1 not 0
-            return [left + 1, right + 1]
+            elif currentSum < target:
+                leftPointer += 1
+
+            else:
+                rightPointer -= 1
