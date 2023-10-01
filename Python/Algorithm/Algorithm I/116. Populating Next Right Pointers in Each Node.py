6816149116ce8e89c532114e577eb2582613f005
Nitzan <nitzantomer1998@gmail.com>
2023-10-01 09:41:33 +0300

    Adding Notes

--- Python/Algorithm/Algorithm I/116. Populating Next Right Pointers in Each Node.py
+++ Python/Algorithm/Algorithm I/116. Populating Next Right Pointers in Each Node.py
@@ -1,16 +1,5 @@
-from typing import Optional
 import collections
-
-
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
+from typing import Optional
 
 
 class Solution:
@@ -23,6 +12,9 @@ class Solution:
 
         Returns:
             Optional[Node]: The root node of the modified binary tree with next pointers.
+
+        Time Complexity: o(n) where n is the number of nodes in the binary tree.
+        Space Complexity: o(n) where n is the number of nodes in the binary tree.
         """
         queue = collections.deque([root])
 
