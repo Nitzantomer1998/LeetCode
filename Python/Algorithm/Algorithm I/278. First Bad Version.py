1321618e9083f0d0f375e9f675c8c75cb28bd5ee
Nitzan <nitzantomer1998@gmail.com>
2023-10-13 09:10:03 +0300

    Adding Notes

--- Python/Algorithm/Algorithm I/278. First Bad Version.py
+++ Python/Algorithm/Algorithm I/278. First Bad Version.py
@@ -1,40 +1,26 @@
-# The isBadVersion API is already defined for you.
-# def isBadVersion(version: int) -> bool:
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        """
+        Find the first bad version in a series of versions using binary search.
 
-def first_bad_version(version_list_size: int) -> int:
-    """
-    Finding the first bad version in our "product", and return it index
+        Args:
+            n (int): The number of versions.
 
-    :param version_list_size: Integer represent amount of existed version for the product
-    :return: The index of the first bad version in our "product"
+        Returns:
+            int: The first bad version.
 
-    Time Complexity: o(log(n))
-    Space Complexity: o(1)
-    """
-    # Variable to the solution
-    first_bad_version_solution = 1
+        Time Complexity: o(log(n)) where n is the number of versions.
+        Space Complexity: o(1) since we are not using any additional space.
+        """
+        leftPointer = 1
+        rightPointer = n
 
-    # Pointers to current container we are searching at
-    left, right = 1, version_list_size
+        while leftPointer <= rightPointer:
+            middlePointer = leftPointer + (rightPointer - leftPointer) // 2
 
-    # Loop to traverse the existed versions without repeating
-    while left <= right:
+            if isBadVersion(middlePointer):
+                rightPointer = middlePointer - 1
+            else:
+                leftPointer = middlePointer + 1
 
-        # Variable to store the middle of the current container we are searching at
-        middle = (right + left) // 2
-
-        # if we found bad version
-        if isBadVersion(middle):
-
-            # Update the current solution
-            first_bad_version_solution = middle
-
-            # Update the current container and keep searching for earlier bad versions
-            right = middle - 1
-
-        # if we haven't found bad version, then the version container
-        else:
-            left = middle + 1
-
-    # Returning the solution
-    return first_bad_version_solution
+        return rightPointer + 1
