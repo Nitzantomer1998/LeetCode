8c177bb6df7730369debff3e81f2d969d243b243
Nitzan <nitzantomer1998@gmail.com>
2023-10-15 09:07:51 +0300

    Adding Notes

--- Python/Algorithm/Algorithm I/344. Reverse String.py
+++ Python/Algorithm/Algorithm I/344. Reverse String.py
@@ -1,24 +1,27 @@
-def reverse_string(string: list[str]) -> None:
-    """
-    Update the list by reversing the sent string
+from typing import List
 
-    :param string: List of characters
-    :return: None, Everything happen in place
 
-    Time Complexity: o(n)
-    Space Complexity: o(1)
-    """
-    # Pointers to the start and end of the string
-    left, right = 0, len(string) - 1
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Reverse a list of characters in-place.
 
-    # Loop to traverse the list and update it
-    while left < right:
-        # Swapping characters position between the start and end indices
-        string[left], string[right] = string[right], string[left]
+        Args:
+            s (List[str]): A list of characters.
 
-        # Update the pointers, for the next iteration
-        left += 1
-        right -= 1
+        Returns:
+            None: The function modifies the input list in-place.
 
-    # Explicit None return, everything happen in place nothing need to be return
-    return None
+        Time Complexity: o(n) where n is the length of the input list.
+        Space Complexity: o(1) since we are not using any extra space.
+        """
+        STRING_LENGTH = len(s)
+
+        startPointer = 0
+        endPointer = STRING_LENGTH - 1
+
+        while startPointer < endPointer:
+            s[startPointer], s[endPointer] = s[endPointer], s[startPointer]
+
+            startPointer += 1
+            endPointer -= 1
