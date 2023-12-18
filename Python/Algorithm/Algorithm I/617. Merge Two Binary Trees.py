d06cf1b5a09c7083cef13b175732670d3640b3c7
Nitzan <nitzantomer1998@gmail.com>
2023-12-18 12:56:27 +0200

    Adding Notes

--- Python/Algorithm/Algorithm I/617. Merge Two Binary Trees.py
+++ Python/Algorithm/Algorithm I/617. Merge Two Binary Trees.py
@@ -1,46 +1,28 @@
-# Class provided by LeetCode for the following problem
-class TreeNode:
-    def __init__(self, value=0, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+from typing import Optional
 
 
-def merge_trees(root1: TreeNode, root2: TreeNode) -> TreeNode:
-    """
-    Merging the two binary trees into a new binary tree "super" tree, and return it
-
-    :param root1: TreeNode representing binary tree
-    :param root2: TreeNode representing binary tree
-    :return: The merged "super" tree
-
-    Time Complexity: o(max(n, m))
-    Space Complexity: o(max(n + m))
-    """
-
-    # Assisting function to make the DFS calls
-    def recursive_root_creator(root1: TreeNode, root2: TreeNode) -> TreeNode:
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         """
-        Recursive function for creating "super" binary tree from two TreeNode  using DFS Algorithm
+        Merges two binary trees by summing up corresponding nodes.
 
-        :param root1: TreeNode representing binary tree
-        :param root2: TreeNode representing binary tree
-        :return: The "super" tree
+        Args:
+            root1: The root of the first binary tree.
+            root2: The root of the second binary tree.
+
+        Returns:
+            The root of the merged binary tree.
+
+        Time Complexity: o(n), where n is the total number of nodes in the smaller of the two trees.
+        Space Complexity: o(n), where n is the maximum height of the smaller of the two trees.
         """
-        # if both roots exists, combine their values and create a new "super" node
         if root1 and root2:
-            root = TreeNode(root1.value + root2.value)
+            mergedTree = TreeNode(root1.val + root2.val)
+        
+            mergedTree.left = self.mergeTrees(root1.left, root2.left)
+            mergedTree.right = self.mergeTrees(root1.right, root2.right)
 
-            # After creating a root, follow up by 2 recursive callbacks, for adding the left and right children's
-            root.left = recursive_root_creator(root1.left, root2.left)
-            root.right = recursive_root_creator(root1.right, root2.right)
-
-            # Finally return this "super" node
-            return root
-
-        # if one of the nodes exist, it will return it, else will return None
         else:
-            return root1 or root2
+            mergedTree = root1 if root1 else root2
 
-    # Calling for the recursive call which create the "super" tree, and return it
-    return recursive_root_creator(root1, root2)
+        return mergedTree
