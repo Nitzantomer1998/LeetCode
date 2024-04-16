75ce3bb2c5451e6d24ebdb5cbd755ccdc03cc35f
Nitzan Tomer <Nitzantomer1998@gmail.com>
2024-04-16 17:38:48 +0300

    Daily Challenge 129. Sum Root to Leaf Numbers

---
+++ Daily Challenge/129. Sum Root to Leaf Numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        return self.DFS(root, 0)
+
+    def DFS(self, node: Optional[TreeNode], currentSum: int) -> int:
+        if node is None:
+            return 0
+
+        newSum = currentSum * 10 + node.val
+        if node.left is None and node.right is None:
+            return newSum
+
+        return self.DFS(node.left, newSum) + self.DFS(node.right, newSum)
+        
