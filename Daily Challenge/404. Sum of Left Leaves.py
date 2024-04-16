c9dae49bc11a25dce8d2f00ebe7315b84d341d17
Nitzan Tomer <Nitzantomer1998@gmail.com>
2024-04-16 17:32:10 +0300

    Daily Challenge 404. Sum of Left Leaves

--- Daily Challenge/404. Sum of Left Leaves.py
+++ Daily Challenge/404. Sum of Left Leaves.py
@@ -2,11 +2,11 @@ class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         return self.DFS(root, False)
 
-    def DFS(self, root: Optional[TreeNode], isLeft: bool) -> int:
-        if root is None:
+    def DFS(self, node: Optional[TreeNode], isLeft: bool) -> int:
+        if node is None:
             return 0
 
-        if isLeft and root.left is None and root.right is None:
-            return root.val
+        if isLeft and node.left is None and node.right is None:
+            return node.val
 
-        return self.DFS(root.left, True) + self.DFS(root.right, False)
+        return self.DFS(node.left, True) + self.DFS(node.right, False)
